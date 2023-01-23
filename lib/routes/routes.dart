736f07a2e66ecef456e010496175113c5f15dba2
harol calzada <harolcalzada@MacBook-Pro-de-harol.local>
2023-01-23 03:11:41 -0500

    add slider and fix show password

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,9 @@
 import 'package:finniu/screens/intro_screen.dart';
 import 'package:finniu/screens/login/email_screen.dart';
+<<<<<<< Updated upstream
+=======
+import 'package:finniu/screens/signup/email_screen.dart';
+>>>>>>> Stashed changes
 import 'package:finniu/screens/login/start_screen.dart';
 import 'package:flutter/material.dart';
 // import 'package:teve_empresa_app/src/pages/chapter.dart';
@@ -13,6 +17,10 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     '/': (BuildContext context) => IntroScreen(),
     '/login_start': (BuildContext context) => StartLoginScreen(),
     '/login_email': (BuildContext context) => EmailLoginScreen(),
+<<<<<<< Updated upstream
+=======
+    '/sign_up_email': (BuildContext context) => SignUpEmailScreen(),
+>>>>>>> Stashed changes
     // '/home': (BuildContext context) => HomePage(),
     // '/chapter': (BuildContext context) => ChapterPage(),
     // '/program_list': (BuildContext context) => ProgramListPage(),
