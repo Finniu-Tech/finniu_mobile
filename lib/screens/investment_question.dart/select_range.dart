9f0bc1b2a1354cf7e77fc26d94ffaf077cfbd754
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid.local>
2023-02-03 22:34:23 -0500

    select range

--- lib/screens/investment_question.dart/select_range.dart
+++ lib/screens/investment_question.dart/select_range.dart
@@ -1,4 +1,5 @@
 import 'package:finniu/constants/colors.dart';
+import 'package:finniu/screens/login/start_screen.dart';
 import 'package:finniu/widgets/fonts.dart';
 import 'package:finniu/widgets/scaffold.dart';
 import 'package:flutter/material.dart';
@@ -13,35 +14,177 @@ class SelectRange extends StatefulWidget {
 class _SelectRangeState extends State<SelectRange> {
   @override
   Widget build(BuildContext context) {
+    int selectedIndex;
     return CustomScaffoldReturnLogo(
-        body: Center(
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-              SizedBox(height: 90),
-            ],
-          ),
-          Stack(alignment: Alignment.centerRight, children: <Widget>[
-            const TextPoppins(
-              text: 'Selecciona tu rango de edad ',
-              colorText: primaryDark,
-              fontSize: 24,
-              fontWeight: FontWeight.w600,
-            ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
             Row(
-              children: [
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                SizedBox(height: 90),
+              ],
+            ),
+            Stack(alignment: Alignment.centerRight, children: <Widget>[
+              const TextPoppins(
+                text: 'Selecciona tu rango de edad ',
+                colorText: primaryDark,
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: Image.asset('assets/investment/star.png'),
+                  ),
+                ],
+              ),
+            ]),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
                 SizedBox(
-                  width: 40,
-                  height: 40,
-                  child: Image.asset('assets/investment/star.png'),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SelectRange()),
+                      );
+                    },
+                    child: Container(
+                      width: 320,
+                      height: 53,
+                      decoration: BoxDecoration(
+                        color: const Color(primaryLightAlternative),
+                        border: Border.all(
+                          color: const Color(primaryLightAlternative),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Center(
+                        child: TextPoppins(
+                          text: '18-25',
+                          colorText: primaryDark,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-          ]),
-        ],
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 320,
+              height: 53,
+              child: InkWell(
+                onTap: () => setState(() => selectedIndex = 1),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(primaryLightAlternative),
+                    border: Border.all(
+                      color: const Color(primaryLightAlternative),
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Center(
+                    child: TextPoppins(
+                      text: '25-35',
+                      colorText: primaryDark,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 320,
+              height: 53,
+              child: InkWell(
+                onTap: () => setState(() => selectedIndex = 1),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(primaryLightAlternative),
+                    border: Border.all(
+                      color: const Color(primaryLightAlternative),
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Center(
+                    child: TextPoppins(
+                      text: '35-45',
+                      colorText: primaryDark,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 320,
+              height: 53,
+              child: InkWell(
+                onTap: () => setState(() => selectedIndex = 1),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(primaryLightAlternative),
+                    border: Border.all(
+                      color: const Color(primaryLightAlternative),
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Center(
+                    child: TextPoppins(
+                      text: '45-50',
+                      colorText: primaryDark,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: 320,
+              height: 53,
+              child: InkWell(
+                onTap: () => setState(() => selectedIndex = 1),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(primaryLightAlternative),
+                    border: Border.all(
+                      color: const Color(primaryLightAlternative),
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Center(
+                    child: TextPoppins(
+                      text: '55-65',
+                      colorText: primaryDark,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
