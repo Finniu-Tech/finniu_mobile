2cbacfb553a0391f226c7ce0d2a6c3adc0fe956a
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid.local>
2023-02-13 16:59:38 -0500

    notification and settings

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,11 @@
 import 'package:finniu/constants/colors.dart';
+import 'package:finniu/services/share_preferences_service.dart';
 import 'package:finniu/widgets/buttons.dart';
 import 'package:finniu/widgets/cardtable.dart';
+import 'package:finniu/widgets/fonts.dart';
 import 'package:finniu/widgets/scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 class HomeStart extends StatelessWidget {
   HomeStart({super.key});
@@ -10,74 +13,247 @@ class HomeStart extends StatelessWidget {
 
   void _showSettingsDialog(BuildContext ctx) {
     showDialog(
-      context: ctx,
-      builder: (ctx) => Dialog(
-        backgroundColor: Colors.orange,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Container(
-          height: 200,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  icon: Container(
-                    height: 23,
-                    width: 23,
-                    decoration: BoxDecoration(
-                      color: Color(primaryDark),
-                      borderRadius: BorderRadius.circular(7),
-                    ),
-                    child: Icon(
-                      size: 20,
-                      Icons.close,
-                      color: Colors.white,
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.of(ctx).pop();
-                  },
-                ),
-              ),
-              Text("Alerta"),
-              Text("¿Estas seguro de cerrar sesión?"),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(ctx).pop();
-                },
-                child: Text("Cancelar"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(ctx).pop();
-                },
-                child: Text("Aceptar"),
-              ),
-            ],
-          ),
-        ),
+        context: ctx,
+        builder: (ctx) => Dialog(
+            backgroundColor: Color(secondary),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Container(
+                width: 331,
+                height: 525,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: Container(
+                            height: 23,
+                            width: 23,
+                            decoration: BoxDecoration(
+                              color: Color(primaryDark),
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                            child: const Icon(
+                              size: 20,
+                              Icons.close,
+                              color: Color(secondaryLight),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.of(ctx).pop();
+                          },
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              SizedBox(
+                                child: Image.asset("assets/home/avatar.png"),
+                              )
+                            ],
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(children: [
+                                  const Text(
+                                    "Mari",
+                                    style: TextStyle(
+                                      height: 1.5,
+                                      fontSize: 16,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  TextPoppins(
+                                      text: 'Light mode',
+                                      colorText: Theme.of(ctx).colorScheme.primary.value,
+                                      // colorText: ,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500),
+                                  const SizedBox(width: 5),
+                                  FlutterSwitch(
+                                    width: 45,
+                                    height: 24,
+                                    value: Preferences.isDarkMode,
+                                    inactiveColor: const Color(primaryDark),
+                                    activeColor: const Color(primaryLight),
+                                    inactiveToggleColor: const Color(primaryLight),
+                                    activeToggleColor: const Color(primaryDark),
+                                    onToggle: (value) {
+                                      // Preferences.isDarkMode = value;
+                                      // value ? themeProvider.setDarkMode() : themeProvider.setLightMode();
+                                      // setState(() {});
+                                    },
+                                  ),
+                                ]),
+                                const SizedBox(
+                                  height: 7,
+                                ),
+                                Row(
+                                  children: const [
+                                    Text(
+                                      "MarianaCPolo@gmail.com",
+                                      style: TextStyle(
+                                        height: 1.5,
+                                        fontSize: 12,
+                                        color: Color(secondaryLight),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 22,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: const [
+                              Text(
+                                "S/4050",
+                                style: TextStyle(
+                                  height: 1.5,
+                                  fontSize: 11,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Text(
+                                "Total invertido",
+                                style: TextStyle(
+                                  height: 1.5,
+                                  fontSize: 14,
+                                  color: Color(primaryDark),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Container(
+                            height: 60,
+                            width: 2,
+                            color: Color(primaryDark),
+                          ),
+                          Column(
+                            children: const [
+                              Text(
+                                "2",
+                                style: TextStyle(
+                                  height: 1.5,
+                                  fontSize: 11,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Text(
+                                "Planes invertidos",
+                                style: TextStyle(
+                                  height: 1.5,
+                                  fontSize: 14,
+                                  color: Color(primaryDark),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: const [
+                              Icon(Icons.person_outline),
+                              SizedBox(width: 10),
+                              Text("Editar mi perfil"),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: const [
+                              Icon(Icons.privacy_tip_outlined),
+                              SizedBox(width: 10),
+                              Text("Privacidad"),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.model_training),
+                              SizedBox(width: 10),
+                              Text("Mis transferencias"),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: const [
+                              Icon(Icons.g_translate),
+                              SizedBox(width: 10),
+                              Text("Lenguajes"),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: const [
+                              Icon(Icons.help_outline),
+                              SizedBox(width: 10),
+                              Text("Ayuda"),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: const [
+                              Icon(Icons.logout),
+                              SizedBox(width: 10),
+                              Text("Cerrar sesión"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
 
-        // title: Text("Alerta"),
-        // content: Text("¿Estas seguro de cerrar sesión?"),
-        // actions: [
-        //   TextButton(
-        //     onPressed: () {
-        //       Navigator.of(ctx).pop();
-        //     },
-        //     child: Text("Cancelar"),
-        //   ),
-        //   TextButton(
-        //     onPressed: () {
-        //       Navigator.of(ctx).pop();
-        //     },
-        //     child: Text("Aceptar"),
-        //   ),
-        // ],
-      ),
-    );
+                    // title: Text("Alerta"),
+                    // content: Text("¿Estas seguro de cerrar sesión?"),
+                    // actions: [
+                    //   TextButton(
+                    //     onPressed: () {
+                    //       Navigator.of(ctx).pop();
+                    //     },
+                    //     child: Text("Cancelar"),
+                    //   ),
+                    //   TextButton(
+                    //     onPressed: () {
+                    //       Navigator.of(ctx).pop();
+                    //     },
+                    //     child: Text("Aceptar"),
+                    //   ),
+                    // ],
+                  ),
+                ))));
   }
 
   void _showWelcomeModal(BuildContext ctx) {
