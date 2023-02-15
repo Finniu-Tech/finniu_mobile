775b2701197a1986ce848d6ebb6a742cbcef0f98
harol calzada <harolcalzada@MacBook-Pro-de-harol.local>
2023-02-15 17:47:39 -0500

    nueva rama

---
+++ lib/screens/my_investment/my_investment_start.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InvestmentStart extends StatelessWidget {
+  const InvestmentStart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
