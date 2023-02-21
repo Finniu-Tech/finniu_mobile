e862bafe626c545ba50891ca5a5dbc7bd16b0ca9
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid.local>
2023-02-20 22:57:42 -0500

    se acabo el cards my investment

--- lib/screens/my_investment/widgets/card.dart
+++ lib/screens/my_investment/widgets/card.dart
@@ -11,6 +11,7 @@ class ExpandableCard extends HookWidget {
   final String textTiledCard;
   final String textPercentage;
   final String textinvestment;
+  final String textDeclaration;
   final String textContainer;
   ExpandableCard({
     super.key,
@@ -19,6 +20,7 @@ class ExpandableCard extends HookWidget {
     required this.textPercentage,
     required this.textinvestment,
     required this.textContainer,
+    required this.textDeclaration,
   });
 
   @override
@@ -35,6 +37,7 @@ class ExpandableCard extends HookWidget {
             textPercentage: textPercentage,
             textinvestment: textinvestment,
             textTiledCard: textTiledCard,
+            textDeclaration: textDeclaration,
           ),
           if (!isExpanded.value) ...[
             InitialCardBody(
@@ -58,6 +61,7 @@ class CardCustom extends StatelessWidget {
     required this.textPercentage,
     required this.textinvestment,
     required this.textContainer,
+    required this.textDeclaration,
   }) : super(key: key);
 
   final String image;
@@ -65,6 +69,7 @@ class CardCustom extends StatelessWidget {
   final String textPercentage;
   final String textinvestment;
   final String textContainer;
+  final String textDeclaration;
   final ValueNotifier<bool> isExpanded;
 
   @override
@@ -150,8 +155,7 @@ class CardCustom extends StatelessWidget {
                               // mainAxisAlignment: MainAxisAlignment.end,
                               children: [
                                 Icon(
-                                  Icons
-                                      .monetization_on_outlined, // Icono que deseas utilizar
+                                  Icons.monetization_on_outlined, // Icono que deseas utilizar
                                   size: 21.5, // Tamaño del icono
                                   color: Color(primaryDark), // Color del icono
                                 ),
@@ -181,8 +185,7 @@ class CardCustom extends StatelessWidget {
                           Row(
                             children: [
                               Icon(
-                                Icons
-                                    .currency_exchange_rounded, // Icono que deseas utilizar
+                                Icons.currency_exchange_rounded, // Icono que deseas utilizar
                                 size: 21.5, // Tamaño del icono
                                 color: Color(primaryDark), // Color del icono
                               ),
@@ -209,12 +212,12 @@ class CardCustom extends StatelessWidget {
                           Row(
                             children: [
                               Icon(
-                                Icons
-                                    .percent_sharp, // Icono que deseas utilizar
+                                Icons.percent_sharp, // Icono que deseas utilizar
                                 size: 21.5, // Tamaño del icono
                                 color: Color(primaryDark), // Color del icono
                               ),
                               Text(
+                                // textAlign: TextAlign.end,
                                 "Declaración a la Sunat",
                                 style: TextStyle(
                                   fontSize: 10, // Tamaño de fuente
@@ -223,7 +226,7 @@ class CardCustom extends StatelessWidget {
                               ),
                               SizedBox(width: 5),
                               Text(
-                                textPercentage,
+                                textDeclaration,
                                 style: TextStyle(
                                   fontSize: 12, // Tamaño de fuente
                                   color: Color(primaryDark), // Color de texto
@@ -237,12 +240,8 @@ class CardCustom extends StatelessWidget {
                       ),
                     ],
                   ),
-                  SizedBox(
-                    height: 15,
-                  ),
                   Padding(
-                    padding: EdgeInsets.all(
-                        16.0), // aquí puedes configurar la cantidad de padding
+                    padding: EdgeInsets.all(25.0), // aquí puedes configurar la cantidad de padding
                     child: Center(
                       child: Text(
                         textContainer,
@@ -277,94 +276,110 @@ class InitialCardBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 320,
-      height: 130,
-      decoration: BoxDecoration(
-        // border: Border.all(color: Color(primaryDark), width: 2.0),
-        border: Border(
-          top: BorderSide(color: Color(primaryDark), width: 2.0),
-          left: BorderSide(color: Color(primaryDark), width: 2.0),
-          right: BorderSide(color: Color(primaryDark), width: 2.0),
-          bottom: BorderSide(color: Color(primaryDark), width: 2.0),
-        ),
+        width: 320,
+        height: 130,
+        decoration: BoxDecoration(
+          // border: Border.all(color: Color(primaryDark), width: 2.0),
+          border: Border(
+            top: BorderSide(color: Color(primaryDark), width: 2.0),
+            left: BorderSide(color: Color(primaryDark), width: 2.0),
+            right: BorderSide(color: Color(primaryDark), width: 2.0),
+            bottom: BorderSide(color: Color(primaryDark), width: 2.0),
+          ),
 
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(0),
-          topRight: Radius.circular(0),
-          bottomLeft: Radius.circular(30),
-          bottomRight: Radius.circular(30),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(0),
+            topRight: Radius.circular(0),
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
         ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
           SizedBox(
             height: 10,
           ),
           SizedBox(
-            // height: 20,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 90,
-                  height: 70,
-                  child: Transform.translate(
-                    offset: const Offset(0.0, -30.0),
-                    child: Image.asset(
-                      image,
-                      width: 90, // Establecer el ancho de la imagen
-                      height: 70, // Establecer el alto de la imagen
-                    ),
+              // height: 20,
+              child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 90,
+                height: 70,
+                child: Transform.translate(
+                  offset: const Offset(0.0, -30.0),
+                  child: Image.asset(
+                    image,
+                    width: 90, // Establecer el ancho de la imagen
+                    height: 70, // Establecer el alto de la imagen
                   ),
                 ),
-                SizedBox(width: 10),
-                Container(
+              ),
+              SizedBox(width: 10),
+              Container(
                   width: 82,
                   // height: 34,
                   padding: EdgeInsets.all(3),
                   decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Color(gradient_secondary), width: 2.0),
+                    border: Border.all(color: Color(gradient_secondary), width: 2.0),
                     borderRadius: BorderRadius.all(Radius.circular(10.0)),
                     color: Color(gradient_secondary),
                   ),
-                  child: Center(
-                    child: Text(
-                      "$textinvestment Monto minimo",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 10.0,
+                  child: Column(
+                    children: [
+                      Text(
+                        textinvestment,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12.0,
+                        ),
                       ),
-                    ),
+                      Text(
+                        "Monto minimo",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ],
+                  )),
+              SizedBox(width: 12),
+              Container(
+                width: 82,
+                // height: 34,
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(primaryLightAlternative), width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Color(primaryLightAlternative),
+                ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        textPercentage,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12.0,
+                        ),
+                      ),
+                      Text(
+                        "Retorno Anual",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(width: 12),
-                Container(
-                  width: 82,
-                  // height: 34,
-                  padding: EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Color(primaryLightAlternative), width: 2.0),
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    color: Color(primaryLightAlternative),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "$textPercentage Retorno anual",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 10.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+              ),
+            ],
+          )),
           Container(
             height: 32,
             width: 152,
@@ -374,15 +389,9 @@ class InitialCardBody extends StatelessWidget {
               color: Color(primaryDark),
             ),
             child: Center(
-              child: CustomButton(
-                  colorBackground: (primaryDark),
-                  text: "Comenzar a invertir",
-                  colorText: (whiteText),
-                  pushName: '/home_home'),
+              child: CustomButton(colorBackground: (primaryDark), text: "Comenzar a invertir", colorText: (whiteText), pushName: '/home_home'),
             ),
           )
-        ],
-      ),
-    );
+        ]));
   }
 }
