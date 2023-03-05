8d00ea99f445022b509ff215982ab21ee5c19758
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid.local>
2023-03-05 15:59:05 -0500

    mejorando el step 2

--- lib/screens/investment_confirmation/step_2.dart
+++ lib/screens/investment_confirmation/step_2.dart
@@ -19,115 +19,268 @@ class Step_2 extends ConsumerWidget {
     var MontoController;
 
     return CustomScaffoldReturnLogo(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const StepBar(),
-            const SizedBox(height: 20),
-            Container(
-              alignment: Alignment.centerLeft,
-              width: 310,
-              height: 70,
-              child: Text(
-                'Plan Origen',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Color(Theme.of(context).colorScheme.secondary.value),
-                ),
+        body: SingleChildScrollView(
+            child: Column(children: <Widget>[
+      const StepBar(),
+      const SizedBox(height: 20),
+      Container(
+        alignment: Alignment.centerLeft,
+        width: 310,
+        height: 70,
+        child: Text(
+          'Plan Origen',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color(Theme.of(context).colorScheme.secondary.value),
+          ),
+        ),
+      ),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 59.49,
+            height: 31.15,
+            // padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color(primaryDark),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Color(primaryDark),
+                width: 1,
               ),
             ),
-            Stack(
-              children: <Widget>[
-                Container(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: CircularCountdown(),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(top: 10.0, right: 50),
-                            child: Container(
-                              alignment: Alignment.center,
-                              height: 60,
-                              width: 116,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Color(primaryLight),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'S/550',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(primaryDark),
-                                    ),
-                                  ),
-                                  Text(
-                                    'Tu monto invertido',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      color: Color(blackText),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 10.0, right: 50),
-                            child: Container(
-                              alignment: Alignment.center,
-                              height: 60,
-                              width: 116,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Color(secondary),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'S/583',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(primaryDark),
-                                    ),
-                                  ),
-                                  Text(
-                                    'Monto que recibiras',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      color: Color(blackText),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  '6%',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Color(primaryLight),
+                  ),
+                ),
+                Text(
+                  textAlign: TextAlign.center,
+                  'Rentabilidad',
+                  style: TextStyle(
+                    fontSize: 5,
+                    color: Color(primaryLight),
                   ),
                 ),
               ],
             ),
+          ),
+        ],
+      ),
+      Stack(
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CircularCountdown(),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0, right: 50),
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 60,
+                        width: 116,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(primaryLight),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'S/550',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color(primaryDark),
+                              ),
+                            ),
+                            Text(
+                              'Tu monto invertido',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Color(blackText),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0, right: 50),
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 60,
+                        width: 116,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(secondary),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'S/583',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color(primaryDark),
+                              ),
+                            ),
+                            Text(
+                              'Monto que recibiras',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Color(blackText),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      SizedBox(height: 20),
+      Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Elige tu banco',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Color(Theme.of(context).colorScheme.secondary.value),
+                ),
+              ),
+            ),
+            Image.asset('assets/result/money.png', width: 20.0, height: 20),
+            const Spacer(),
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/Alert1 ');
+              },
+              child: Icon(
+                Icons.quiz_outlined, // Icono que deseas utilizar
+                size: 20, // Tamaño del icono
+                color: Color(primaryDark), // Color del icono
+              ),
+            ),
           ],
         ),
       ),
-    );
+      Container(
+        width: 320,
+        height: 71,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(21),
+            bottomLeft: Radius.circular(4),
+            bottomRight: Radius.circular(4),
+          ),
+          color: Colors.white,
+          border: Border.all(
+            color: Color(primaryDark),
+            width: 1,
+          ),
+        ),
+        // padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          // child: Padding(
+          //   padding: const EdgeInsets.all(4.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(Icons.aspect_ratio_sharp),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: 190,
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      'Banco donde realizamos tu transferencia',
+                      style: TextStyle(color: Color(primaryDark), fontWeight: FontWeight.bold),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(primaryDark),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(10),
+                        )),
+                    width: 54,
+                    height: 71,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.loupe_rounded, size: 30, color: Color(primaryLight)),
+                        Text(
+                          textAlign: TextAlign.center,
+                          'Agregar cuenta',
+                          style: TextStyle(fontSize: 8, color: Color(primaryLight)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.only(top: 70.0)),
+            ],
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 30,
+      ),
+      Container(
+        height: 50,
+        width: 224,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Color(primaryDark),
+        ),
+        child: Center(
+          child: CustomButton(colorBackground: currentTheme.isDarkMode ? (primaryLight) : (primaryDark), text: "Continuar", colorText: currentTheme.isDarkMode ? (primaryDark) : (whiteText), pushName: '/investment_step1'),
+        ),
+      ),
+    ])));
   }
 }
 
