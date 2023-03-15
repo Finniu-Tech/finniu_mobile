8d77d95036f8cae4903fbfb9108d71afa242402c
Ingrid Calzada <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2023-03-15 15:06:34 -0500

    se hizo el simulador

--- lib/presentation/screens/calculator/calculator_screen.dart
+++ lib/presentation/screens/calculator/calculator_screen.dart
@@ -11,14 +11,14 @@ class Calculator extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
-  final themeProvider = ref.watch(settingsNotifierProvider);
-    final showError = useState(false);
-    final namesController = useTextEditingController();
-    final docNumberController = useTextEditingController();
-    final departmentController = useTextEditingController();
-    final districtController = useTextEditingController();
-    final addressController = useTextEditingController();
-    final civilStateController = useTextEditingController();
+   final currentTheme = ref.watch(settingsNotifierProvider);
+   final themeProvider = ref.watch(settingsNotifierProvider);
+   
+   final showError = useState(false);
+    final rentabilityController = useTextEditingController();
+    
+    final termController = useTextEditingController();
+    final amountController = useTextEditingController();
 
     final _formKey = GlobalKey<FormState>();
 
@@ -29,23 +29,52 @@ class Calculator extends HookConsumerWidget {
 
 
 
+List<Color> dayColors = [Color (gradient_primary), Color (gradient_secondary)];
+List<Color> nightColors = [Color(primaryDark), Color (primaryLight)]; 
+
+
+
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(50.0),
-      child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-      
-        Row(
+       body: Container(alignment: Alignment.centerRight,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: currentTheme.isDarkMode ? nightColors : dayColors)
+
+      ),
+    
+  
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  
+                  child: Image.asset(
+                    'assets/images/logo_small.png',
+                    width: 70,
+                    height:70,
+                    color:currentTheme.isDarkMode
+                  ? const Color(whiteText)
+                  : const Color(blackText),
+                  ),
+                ),
+              ),
+
+        Row(mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
               child: Container(
-                alignment: Alignment.topCenter,
+                alignment: Alignment.center,
                 child: Container(width :MediaQuery.of(context).size.width * 0.8,
-                  child: Text(
-                    'Simula tu inversion de manera rapida y sencilla',
+                  child: Text(textAlign:TextAlign.center,
+                    'Simula tu inversión de manera rápida y sencilla',
                     style: TextStyle(
                       fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                      color:Color(primaryDark),
+                      fontWeight: FontWeight.bold,
+                      color:currentTheme.isDarkMode
+                  ? const Color(primaryLight)
+                  : const Color(primaryDark),
                     ),
                   ),
                 ),
@@ -55,17 +84,24 @@ class Calculator extends HookConsumerWidget {
           ],
         ),
     
+           Container(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  child: Image.asset(
+                    'assets/welcome/welcome1.png',
+                    width: 180,
+                    height: 260,
+                  ),
+                ),
+              ),
 
-        SizedBox(
-          height: 15.0,
-        ),
-       
+
           SizedBox(
                   width: 224,
                   // height: 38,
                   child: TextFormField(
                     key: const Key('address'),
-                    controller: addressController,
+                    controller: amountController,
                     onChanged: (value) {
                       _calculatePercentage('address');
                     },
@@ -97,166 +133,189 @@ class Calculator extends HookConsumerWidget {
                     ),
                   ),
                 ],
-                const SizedBox(height: 28),
-                SizedBox(
+              
+            const SizedBox(height: 15),
+                 SizedBox(
                   width: 224,
-                  height: 38,
+                  height: 39,
                   child: DropdownSearch<String>(
-                    selectedItem: districtController.text != '' ? districtController.text : null,
-                    key: const Key('plazo'),
+                    selectedItem: amountController.text != '' ?  amountController.text : null,
+                    key: const Key('term'),
                     onChanged: (value) {
-                      districtController.text = value.toString();
-                      _calculatePercentage('plazo');
+                    amountController.text = value.toString();
+                      _calculatePercentage('term');
                     },
                     dropdownDecoratorProps: const DropDownDecoratorProps(
                       dropdownSearchDecoration: InputDecoration(
                         labelText: 'Plazo',
+                         hintText: 'Seleccione su plazo de inversion',
                       ),
                     ),
-                    items: [
-                      '6 meses ',
-                      '12 meses',
-                   
-                    ],
+                    items: ['6 meses ', '12 meses'],
                     popupProps: PopupProps.menu(
                       showSelectedItems: true,
                       itemBuilder: (context, item, isSelected) => Container(
                         decoration: BoxDecoration(
-                          color: Color(isSelected ? primaryDark : primaryLight),
+                          color: Color(
+                            isSelected ? (themeProvider.isDarkMode ? primaryLight : primaryDarkAlternative) : (themeProvider.isDarkMode ? primaryDarkAlternative : primaryLight),
+                          ),
                           borderRadius: const BorderRadius.all(
                             Radius.circular(20),
                           ),
+                          border: Border.all(
+                            width: 2,
+                            color: themeProvider.isDarkMode ? const Color(primaryDarkAlternative) : const Color(primaryLight), // Aquí especificas el color de borde deseado
+                          ),
                         ),
                         padding: const EdgeInsets.all(15),
-                        margin: const EdgeInsets.only(top: 5, bottom: 5, right: 15, left: 15),
+                        margin: const EdgeInsets.only(
+                          top: 5,
+                          bottom: 5,
+                          right: 15,
+                          left: 15,
+                        ),
                         child: Text(
                           item.toString(),
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                            color: Color(isSelected ? Colors.white.value : primaryDark),
+                            color: Color(
+                              isSelected ? (themeProvider.isDarkMode ? primaryDark : Colors.white.value) : (themeProvider.isDarkMode ? Colors.white.value : primaryDark),
+                            ),
                             fontWeight: FontWeight.w500,
                           ),
                         ),
                       ),
-                      menuProps: const MenuProps(
-                        backgroundColor: Color(primaryLightAlternative),
+                      menuProps: MenuProps(
+                        backgroundColor: Color(
+                          themeProvider.isDarkMode ? primaryDark : primaryLightAlternative,
+                        ),
                         shape: RoundedRectangleBorder(
                           side: BorderSide(
-                            color: Color(primaryDark),
+                            color: Color(
+                              themeProvider.isDarkMode ? primaryLight : primaryDark,
+                            ),
                             width: 1.0,
                           ),
-                          borderRadius: BorderRadius.vertical(
+                          borderRadius: const BorderRadius.vertical(
                             top: Radius.circular(10),
                           ),
                         ),
                       ),
                       showSearchBox: true,
-                      searchFieldProps: const TextFieldProps(
+                      searchFieldProps: TextFieldProps(
                         decoration: InputDecoration(
                           filled: true,
-                          fillColor: Colors.white,
+                          fillColor: Theme.of(context).backgroundColor,
                           suffixIcon: Icon(Icons.search),
                           label: Text('Buscar'),
                         ),
                       ),
                     ),
-                    dropdownButtonProps: const DropdownButtonProps(
-                      color: Color(primaryDark),
+                    dropdownButtonProps: DropdownButtonProps(
+                      color: Color(
+                        themeProvider.isDarkMode ? primaryLight : primaryDark,
+                      ),
                       padding: EdgeInsets.zero,
                     ),
                   ),
                 ),
                 const SizedBox(height: 15),
-                if (showError.value) ...[
-                  const Text(
-                    'No se pudo completar el registro',
-                    style: TextStyle(
-                      fontSize: 11,
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-                const SizedBox(height: 15),
               
                 const SizedBox(height: 15),
-                SizedBox(
+               SizedBox(
                   width: 224,
-                  height: 38,
+                  height: 39,
                   child: DropdownSearch<String>(
-                    selectedItem: civilStateController.text != '' ? civilStateController.text : null,
-                    key: const Key('selectionrentability'),
+                    selectedItem: rentabilityController.text != '' ?  rentabilityController.text : null,
+                    key: const Key('rentability'),
                     onChanged: (value) {
-                      civilStateController.text = value.toString();
-                      _calculatePercentage('selectionrentability');
+                    rentabilityController.text = value.toString();
+                      _calculatePercentage('rentability');
                     },
                     dropdownDecoratorProps: const DropDownDecoratorProps(
                       dropdownSearchDecoration: InputDecoration(
                         labelText: 'Eleccion de Rentabilidad',
+                         hintText: 'Seleccione su eleccion',
                       ),
                     ),
-                    items: ['6 meses', '12 meses'],
+                    items: ['Mensual ', 'Plazo Fijo'],
                     popupProps: PopupProps.menu(
                       showSelectedItems: true,
                       itemBuilder: (context, item, isSelected) => Container(
                         decoration: BoxDecoration(
-                          color: Color(isSelected ? primaryDark : primaryLight),
+                          color: Color(
+                            isSelected ? (themeProvider.isDarkMode ? primaryLight : primaryDarkAlternative) : (themeProvider.isDarkMode ? primaryDarkAlternative : primaryLight),
+                          ),
                           borderRadius: const BorderRadius.all(
                             Radius.circular(20),
                           ),
+                          border: Border.all(
+                            width: 2,
+                            color: themeProvider.isDarkMode ? const Color(primaryDarkAlternative) : const Color(primaryLight), // Aquí especificas el color de borde deseado
+                          ),
                         ),
                         padding: const EdgeInsets.all(15),
-                        margin: const EdgeInsets.only(top: 5, bottom: 5, right: 15, left: 15),
+                        margin: const EdgeInsets.only(
+                          top: 5,
+                          bottom: 5,
+                          right: 15,
+                          left: 15,
+                        ),
                         child: Text(
                           item.toString(),
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                            color: Color(isSelected ? Colors.white.value : primaryDark),
+                            color: Color(
+                              isSelected ? (themeProvider.isDarkMode ? primaryDark : Colors.white.value) : (themeProvider.isDarkMode ? Colors.white.value : primaryDark),
+                            ),
                             fontWeight: FontWeight.w500,
                           ),
                         ),
                       ),
-                      menuProps: const MenuProps(
-                        backgroundColor: Color(primaryLightAlternative),
+                      menuProps: MenuProps(
+                        backgroundColor: Color(
+                          themeProvider.isDarkMode ? primaryDark : primaryLightAlternative,
+                        ),
                         shape: RoundedRectangleBorder(
                           side: BorderSide(
-                            color: Color(primaryDark),
+                            color: Color(
+                              themeProvider.isDarkMode ? primaryLight : primaryDark,
+                            ),
                             width: 1.0,
                           ),
-                          borderRadius: BorderRadius.vertical(
+                          borderRadius: const BorderRadius.vertical(
                             top: Radius.circular(10),
                           ),
                         ),
                       ),
                       showSearchBox: true,
-                      searchFieldProps: const TextFieldProps(
+                      searchFieldProps: TextFieldProps(
                         decoration: InputDecoration(
                           filled: true,
-                          fillColor: Colors.white,
+                          fillColor: Theme.of(context).backgroundColor,
                           suffixIcon: Icon(Icons.search),
                           label: Text('Buscar'),
                         ),
                       ),
                     ),
-                    dropdownButtonProps: const DropdownButtonProps(
-                      color: Color(primaryDark),
+                    dropdownButtonProps: DropdownButtonProps(
+                      color: Color(
+                        themeProvider.isDarkMode ? primaryLight : primaryDark,
+                      ),
                       padding: EdgeInsets.zero,
                     ),
                   ),
                 ),
-                const SizedBox(height: 15),
-                if (showError.value) ...[
-                  const Text(
-                    'No se pudo completar el registro',
-                    style: TextStyle(
-                      fontSize: 11,
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
                 Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  child: const CustomButton(text: 'Continuar', colorBackground: primaryDark, colorText: whiteText, pushName: '/home_home'),
-                ),
+                  margin:  EdgeInsets.only(top: 20),
+                  child: CustomButton(text: 'Continuar', colorBackground: currentTheme.isDarkMode
+                  ? (primaryLight)
+                  :  primaryDark,     colorText: currentTheme.isDarkMode
+                  ? (primaryDark)
+                  :  whiteText, pushName: '/home_home'), ),
+                  
+             
+       
       
       
       
