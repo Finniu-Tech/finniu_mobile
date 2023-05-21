284a5658eaeb1323242de57ce55c25383f4453ff
harol calzada <harolcalzada@MacBook-Pro-de-harol.local>
2023-05-21 02:14:30 -0500

    finish integration change password logued

---
+++ lib/domain/datasources/change_password_logued.dart
@@ -0,0 +1,10 @@
+import 'package:finniu/graphql/mutations.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+abstract class ChangePasswordLoguedDataSource {
+  Future<bool> changePasswordLogued({
+    required GraphQLClient client,
+    required String oldPassword,
+    required String newPassword,
+  });
+}
