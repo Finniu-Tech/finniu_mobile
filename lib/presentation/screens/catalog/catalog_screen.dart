1a62ef5c8e52ba920b582cc2c1cd0331043bc3c9
franco galbiati <francogalbiati984@gmail.com>
2024-07-04 16:21:44 -0300

    add (non_investment) : NonInvestmentContainer is added in catalog widgets and home_screen (more stak and logic to render)

--- lib/presentation/screens/catalog/catalog_screen.dart
+++ lib/presentation/screens/catalog/catalog_screen.dart
@@ -4,6 +4,7 @@ import 'package:finniu/presentation/screens/catalog/widgets/graphic_container.da
 import 'package:finniu/presentation/screens/catalog/widgets/header_investment.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/image_container.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/send_proof_button.dart';
+import 'package:finniu/presentation/screens/home_v2/widgets/non_investmenr.dart';
 import 'package:finniu/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -25,10 +26,12 @@ class CatalogScreen extends HookConsumerWidget {
           children: [
             ElevatedButton(
               onPressed: () => Navigator.pushNamed(context, '/home_home'),
-              child: Text('Ir a home normal'),
+              child: const Text('Ir a home normal'),
             ),
             TextButton(
-                onPressed: () => Navigator.pushNamed(context, '/catalog'), child: Text('Ver Catalogo de Widgets')),
+              onPressed: () => Navigator.pushNamed(context, '/catalog'),
+              child: const Text('Ver Catalogo de Widgets'),
+            ),
             const ButtonSendProof(),
             const BlueGoldImage(),
             const CarrouselSlide(),
@@ -39,7 +42,8 @@ class CatalogScreen extends HookConsumerWidget {
               iconColor: aboutIconBusinessColor,
               textColor: aboutTextBusinessColor,
               urlIcon: 'assets/investment/business_loans_investment_icon.png',
-              urlImageBackground: 'assets/backgroud/image-inmobiliaria-backgroud.png',
+              urlImageBackground:
+                  'assets/backgroud/image-inmobiliaria-backgroud.png',
               textTitle: 'Fondo prestamos empresariales',
             ),
             const SizedBox(height: 10),
@@ -52,6 +56,7 @@ class CatalogScreen extends HookConsumerWidget {
               textTitle: 'Fondo inversión agro inmobiliaria',
             ),
             const SizedBox(height: 10),
+            const NonInvestmentContainer(),
           ],
         ),
       ),
