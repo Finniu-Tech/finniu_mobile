8ca68553baef2905c0dae3b2be4edba1cf9b74cf
franco galbiati <francogalbiati984@gmail.com>
2024-07-29 15:18:48 -0300

    add ( investment_screen ) : add title expanded and navigate

--- lib/presentation/screens/investment_v2/investment_screen_v2.dart
+++ lib/presentation/screens/investment_v2/investment_screen_v2.dart
@@ -11,75 +11,118 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 class PageWidget {
   Widget title;
   Widget itemBuilder;
-  PageWidget({required this.itemBuilder, required this.title});
+  PageWidget({
+    required this.itemBuilder,
+    required this.title,
+  });
 }
 
-class InvestmentsV2Screen extends HookConsumerWidget {
+class InvestmentsV2Screen extends StatefulWidget {
   const InvestmentsV2Screen({super.key});
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
-    const int columnColorDark = 0xff0E0E0E;
-    const int columnColorLight = 0xffF8F8F8;
-    PageController pageController = PageController();
-    List<PageWidget> itemBuilder = [
-      PageWidget(
-        title: GestureDetector(
-          onTap: () => pageController.animateToPage(
-            0,
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.ease,
-          ),
-          child: const EnterpriseFundTitle(),
-        ),
-        itemBuilder: const RealEstateBody(),
-      ),
-      PageWidget(
-        title: GestureDetector(
-          onTap: () => pageController.animateToPage(
-            1,
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.ease,
-          ),
-          child: const BlueGoldFundTitle(),
-        ),
-        itemBuilder: const BlueGoldBody(),
-      ),
-    ];
 
-    return Scaffold(
-      backgroundColor: isDarkMode
-          ? const Color(columnColorDark)
-          : const Color(columnColorLight),
-      appBar: const AppBarBusinessScreen(),
-      bottomNavigationBar: const NavigationBarHome(
-        colorBackground: Colors.transparent,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 50,
-              child: Row(
-                children: [itemBuilder[0].title, itemBuilder[1].title],
+  @override
+  InvestmentsV2ScreenState createState() => InvestmentsV2ScreenState();
+}
+
+class InvestmentsV2ScreenState extends State<InvestmentsV2Screen> {
+  final PageController pageController = PageController();
+  int selectedPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return HookConsumer(
+      builder: (context, ref, _) {
+        final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+        const int columnColorDark = 0xff0E0E0E;
+        const int columnColorLight = 0xffF8F8F8;
+
+        List<PageWidget> pageWidgets = [
+          PageWidget(
+            title: GestureDetector(
+              onTap: () {
+                pageController.animateToPage(
+                  0,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.ease,
+                );
+                setState(() {
+                  selectedPage = 0;
+                });
+              },
+              child: RealStateTitleAndNavigate(
+                isSelect: selectedPage == 0,
+                isDarkMode: isDarkMode,
               ),
             ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.9,
-              child: PageView.builder(
-                itemCount: itemBuilder.length,
-                itemBuilder: (context, index) {
-                  return itemBuilder[index].itemBuilder;
-                },
-                controller: pageController,
+            itemBuilder: const RealEstateBody(),
+          ),
+          PageWidget(
+            title: GestureDetector(
+              onTap: () {
+                pageController.animateToPage(
+                  1,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.ease,
+                );
+                setState(() {
+                  selectedPage = 1;
+                });
+              },
+              child: BlueGoldTitleAndNavigate(
+                isSelect: selectedPage == 1,
+                isDarkMode: isDarkMode,
               ),
             ),
-          ],
-        ),
-      ),
+            itemBuilder: const BlueGoldBody(),
+          ),
+        ];
+
+        return Scaffold(
+          backgroundColor: isDarkMode
+              ? const Color(columnColorDark)
+              : const Color(columnColorLight),
+          appBar: const AppBarBusinessScreen(),
+          bottomNavigationBar: const NavigationBarHome(
+            colorBackground: Colors.transparent,
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 45,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      pageWidgets[0].title,
+                      const SizedBox(width: 7),
+                      pageWidgets[1].title,
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: PageView.builder(
+                    itemCount: pageWidgets.length,
+                    itemBuilder: (context, index) {
+                      return pageWidgets[index].itemBuilder;
+                    },
+                    controller: pageController,
+                    onPageChanged: (index) {
+                      setState(() {
+                        selectedPage = index;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
