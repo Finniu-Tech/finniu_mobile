a5e2a410ec3e24d28609bb38d924af68b2181481
franco galbiati <francogalbiati984@gmail.com>
2024-08-05 16:15:51 -0300

    add ( verification)code_v2 ) : add VerificationCodeV2 and fix styles

--- lib/presentation/screens/activate_account_v2.dart/activate_account_v2.dart
+++ lib/presentation/screens/activate_account_v2.dart/activate_account_v2.dart
@@ -1,17 +1,13 @@
-import 'package:finniu/constants/colors.dart';
-import 'package:finniu/infrastructure/models/otp.dart';
 import 'package:finniu/presentation/providers/graphql_provider.dart';
 import 'package:finniu/presentation/providers/otp_provider.dart';
-import 'package:finniu/presentation/providers/settings_provider.dart';
 import 'package:finniu/presentation/providers/timer_counterdown_provider.dart';
 import 'package:finniu/presentation/providers/user_provider.dart';
+import 'package:finniu/presentation/screens/activate_account_v2.dart/widgets/verification_code_v2.dart';
+import 'package:finniu/presentation/screens/catalog/widgets/send_proof_button.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/user_profil_v2/scafold_user_profil.dart';
-import 'package:finniu/presentation/screens/signup/widgets/counter.dart';
-import 'package:finniu/services/share_preferences_service.dart';
 import 'package:finniu/widgets/snackbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_verification_code/flutter_verification_code.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ActivateAccountV2 extends HookConsumerWidget {
@@ -21,7 +17,6 @@ class ActivateAccountV2 extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final themeProvider = ref.watch(settingsNotifierProvider);
     final user = ref.watch(userProfileNotifierProvider);
     final client = ref.watch(gqlClientProvider);
     const int titleDark = 0xffA2E6FA;
@@ -31,9 +26,9 @@ class ActivateAccountV2 extends HookConsumerWidget {
 
     return ScaffoldUserProfile(children: [
       SizedBox(
-        height: MediaQuery.of(context).size.height * 0.8,
+        height: MediaQuery.of(context).size.height * 0.9,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const TextPoppins(
               text: "Activa tu cuenta en segundos",
@@ -43,10 +38,16 @@ class ActivateAccountV2 extends HookConsumerWidget {
               lines: 2,
               align: TextAlign.center,
             ),
+            const SizedBox(
+              height: 20,
+            ),
             Image.asset(
               'assets/images/padlock.png',
               width: 100,
               height: 100,
+            ),
+            const SizedBox(
+              height: 20,
             ),
             const TextPoppins(
               text: "Ingresa el código de verificación",
@@ -55,6 +56,9 @@ class ActivateAccountV2 extends HookConsumerWidget {
               textLight: subTitleLight,
               isBold: true,
               align: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
             ),
             const TextPoppins(
               text:
@@ -66,9 +70,16 @@ class ActivateAccountV2 extends HookConsumerWidget {
               align: TextAlign.center,
               lines: 2,
             ),
+            const SizedBox(
+              height: 20,
+            ),
             const VerificationCodeV2(),
+            const SizedBox(
+              height: 20,
+            ),
             if (ref.watch(timerCounterDownProvider) == 0) ...[
-              TextButton(
+              ButtonInvestment(
+                text: "Reenviar correo",
                 onPressed: () async {
                   ref.read(timerCounterDownProvider.notifier).resetTimer();
 
@@ -93,103 +104,27 @@ class ActivateAccountV2 extends HookConsumerWidget {
                     loading: () => null,
                   );
                 },
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                  backgroundColor: Color(
-                    themeProvider.isDarkMode ? primaryLight : primaryDark,
-                  ),
-                ),
-                child: Text(
-                  'Reenviar código',
-                  style: TextStyle(
-                    fontSize: 11,
-                    fontWeight: FontWeight.w500,
-                    height: 1.5,
-                    color: themeProvider.isDarkMode
-                        ? const Color(primaryDark)
-                        : Colors.white,
-                  ),
-                ),
               ),
               const SizedBox(
                 height: 15,
               ),
             ] else ...[
-              const CircularCountdown(),
+              const TextPoppins(
+                text: "Reenviar el código en ",
+                fontSize: 11,
+                textDark: subTitleDark,
+                textLight: subTitleLight,
+                isBold: false,
+                align: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const CircularCountdownV2(),
             ],
           ],
         ),
       )
     ]);
-  }
-}
-
-class VerificationCodeV2 extends HookConsumerWidget {
-  const VerificationCodeV2({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final themeProvider = ref.watch(settingsNotifierProvider);
-    final userProfile = ref.watch(userProfileNotifierProvider);
-
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.transparent,
-          ),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: VerificationCode(
-          fullBorder: true,
-          textStyle: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: themeProvider.isDarkMode
-                ? Colors.white
-                : const Color(primaryDark),
-          ),
-          keyboardType: TextInputType.number,
-          underlineColor: const Color(
-            0xff9381FF,
-          ),
-          underlineUnfocusedColor: Color(
-            themeProvider.isDarkMode ? primaryLight : primaryDark,
-          ),
-          length: 4,
-          itemSize: 50,
-          cursorColor: Colors.blue,
-          onCompleted: (code) {
-            final futureIsValidCode = ref.watch(
-              otpValidatorFutureProvider(
-                OTPForm(
-                  email: userProfile.email!,
-                  otp: code,
-                  action: 'register',
-                ),
-              ).future,
-            );
-            futureIsValidCode.then((status) {
-              if (status == true) {
-                Preferences.username = userProfile.email!;
-                Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  '/on_boarding_start',
-                  (route) => false, // Remove all the previous routes
-                );
-              } else {
-                Navigator.of(context).pop();
-                CustomSnackbar.show(
-                  context,
-                  'No se pudo validar el código de verificación',
-                  'error',
-                );
-              }
-            });
-          },
-          onEditing: (bool value) {},
-        ),
-      ),
-    );
   }
 }
