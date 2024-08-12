3d924bce09ab3e1a063d0cc2d1590d822c84d787
franco galbiati <francogalbiati984@gmail.com>
2024-08-12 15:11:45 -0300

    add svg

--- lib/presentation/screens/home_v2/widgets/show_draft_modal.dart
+++ lib/presentation/screens/home_v2/widgets/show_draft_modal.dart
@@ -1,12 +1,14 @@
 import 'package:finniu/presentation/providers/settings_provider.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
+import 'package:finniu/presentation/screens/home_v2/widgets/slider_draft_modal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void showDraftModal(BuildContext context) {
-  final bool isReinvest = false;
+  final bool isReinvest = true;
   showModalBottomSheet(
+    scrollControlDisabledMaxHeightRatio: 1,
     context: context,
     builder: (context) => _DraftBody(
       isReinvest: isReinvest,
@@ -22,6 +24,18 @@ class _DraftBody extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    void navigate() {
+      if (isReinvest) {
+        print("navegar a reinversion");
+      } else {
+        print("navegar a inversion");
+      }
+    }
+
+    void contact() {
+      print("contact");
+    }
+
     String title = isReinvest ? "¡Hola!👋🏼" : "¡Hola!👋🏼";
     String subTitle = isReinvest
         ? "Parece que dejaste tu reinversión a medias."
@@ -38,7 +52,7 @@ class _DraftBody extends ConsumerWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextPoppins(
@@ -46,17 +60,11 @@ class _DraftBody extends ConsumerWidget {
               fontSize: 20,
               isBold: true,
             ),
-            const SizedBox(
-              height: 10,
-            ),
             TextPoppins(
               text: subTitle,
               fontSize: 20,
               isBold: true,
               lines: 2,
-            ),
-            const SizedBox(
-              height: 10,
             ),
             TextPoppins(
               text: bodyText,
@@ -68,12 +76,150 @@ class _DraftBody extends ConsumerWidget {
               fontSize: 14,
               lines: 2,
             ),
-            const SizedBox(
-              height: 15,
-            ),
             const RowIconStatus(),
+            SliderDraftModal(
+              amountNumber: 100,
+              isReinvest: isReinvest,
+            ),
+            ButtonGoInvest(
+              isReinvest: isReinvest,
+              navigate: () => navigate(),
+            ),
+            ContactAdvisor(
+              contact: () => contact(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContactAdvisor extends ConsumerWidget {
+  const ContactAdvisor({
+    super.key,
+    required this.contact,
+  });
+  final VoidCallback? contact;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    const int dividerDark = 0xffA2E6FA;
+    const int dividerLight = 0xff0D3A5C;
+    const int iconBackgroundDark = 0xff2B2B2B;
+    const int iconBackgroundLight = 0xffEFEFEF;
+
+    return GestureDetector(
+      onTap: contact,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          children: [
+            Expanded(
+              child: Divider(
+                thickness: 2,
+                color: isDarkMode
+                    ? const Color(dividerDark)
+                    : const Color(dividerLight),
+              ),
+            ),
             const SizedBox(
-              height: 10,
+              width: 10,
+            ),
+            SizedBox(
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: isDarkMode
+                          ? const Color(iconBackgroundDark)
+                          : const Color(iconBackgroundLight),
+                    ),
+                    child: SvgPicture.asset(
+                      "assets/svg_icons/chat_icon_draft.svg",
+                      width: 24,
+                      height: 24,
+                      color: isDarkMode
+                          ? const Color(dividerDark)
+                          : const Color(dividerLight),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const TextPoppins(
+                    text: "Contactar con el asesor",
+                    fontSize: 14,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Divider(
+                thickness: 2,
+                color: isDarkMode
+                    ? const Color(dividerDark)
+                    : const Color(dividerLight),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonGoInvest extends ConsumerWidget {
+  const ButtonGoInvest({
+    super.key,
+    required this.isReinvest,
+    required this.navigate,
+  });
+  final bool isReinvest;
+  final VoidCallback navigate;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const String textInvest = "Retomar mi proceso de inversión";
+    const String textReinvest = "Retomar mi proceso de reinversión";
+
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    const int backgroundColorDark = 0xffA2E6FA;
+    const int backgroundColorLight = 0xff0D3A5C;
+    const int textDark = 0xff000000;
+    const int textLight = 0xffFFFFFF;
+    const int iconDark = 0xff08273F;
+    const int iconLight = 0xffA2E6FA;
+    return GestureDetector(
+      onTap: navigate,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: isDarkMode
+              ? const Color(backgroundColorDark)
+              : const Color(backgroundColorLight),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextPoppins(
+              text: isReinvest ? textReinvest : textInvest,
+              fontSize: 14,
+              isBold: true,
+              textDark: textDark,
+              textLight: textLight,
+            ),
+            Icon(
+              Icons.arrow_forward_outlined,
+              color:
+                  isDarkMode ? const Color(iconDark) : const Color(iconLight),
             ),
           ],
         ),
---
+++ lib/presentation/screens/home_v2/widgets/slider_draft_modal.dart
@@ -0,0 +1,237 @@
+import 'package:finniu/presentation/providers/settings_provider.dart';
+import 'package:finniu/presentation/screens/catalog/row_title_amount.dart';
+import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SliderDraftModal extends ConsumerWidget {
+  const SliderDraftModal({
+    super.key,
+    required this.amountNumber,
+    required this.isReinvest,
+  });
+  final int amountNumber;
+  final bool isReinvest;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    const int backgroundDark = 0xff08273F;
+    const int backgroundLight = 0xffD6F6FF;
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: isDarkMode
+            ? const Color(backgroundDark)
+            : const Color(backgroundLight),
+      ),
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 156,
+      child: Stack(
+        children: [
+          const LabelText(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                _RowNumber(
+                  amountNumber: amountNumber,
+                ),
+                const SizedBox(height: 10),
+                _InvestmentData(
+                  isReinvest: isReinvest,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _InvestmentData extends ConsumerWidget {
+  const _InvestmentData({
+    required this.isReinvest,
+  });
+  final bool isReinvest;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    const int backgroundContainer = 0xffA2E6FA;
+    const int textContainer = 0xff000000;
+    const int iconDark = 0xffA2E6FA;
+    const int iconLight = 0xff08273F;
+    const int alertDark = 0xffFFFFFF;
+    const int alertLight = 0xff08273F;
+    const int alertIconDark = 0xffFFFFFF;
+    const int alertIconLight = 0xff000000;
+
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.7,
+      height: 90,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          if (isReinvest)
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(backgroundContainer),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              width: 204,
+              height: 24,
+              child: const Center(
+                child: TextPoppins(
+                  text: "Reinversión con monto agregado",
+                  fontSize: 11,
+                  textDark: textContainer,
+                  textLight: textContainer,
+                ),
+              ),
+            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  SvgPicture.asset(
+                    "assets/svg_icons/calendar_blank.svg",
+                    width: 20,
+                    height: 20,
+                    color: isDarkMode
+                        ? const Color(iconDark)
+                        : const Color(iconLight),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const TextPoppins(text: "Plazo 6 meses", fontSize: 12),
+                ],
+              ),
+              Row(
+                children: [
+                  SvgPicture.asset(
+                    "assets/svg_icons/status_up_icon_draft.svg",
+                    width: 20,
+                    height: 20,
+                    color: isDarkMode
+                        ? const Color(iconDark)
+                        : const Color(iconLight),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const TextPoppins(
+                    text: "14% rentabilidad",
+                    fontSize: 12,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SvgPicture.asset(
+                "assets/svg_icons/alert_circle.svg",
+                width: 20,
+                height: 20,
+                color: isDarkMode
+                    ? const Color(alertIconDark)
+                    : const Color(alertIconLight),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              const TextPoppins(
+                text: "Solo te faltaba realizar la transferencia",
+                fontSize: 11,
+                textDark: alertDark,
+                textLight: alertLight,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _RowNumber extends ConsumerWidget {
+  const _RowNumber({
+    required this.amountNumber,
+  });
+  final int amountNumber;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const int lineColor = 0xffA2E6FA;
+    const int titleColorDark = 0xffFFFFFF;
+    const int titleColorLight = 0xff000000;
+    const int amountColorDark = 0xffA2E6FA;
+    const int amountColorLight = 0xff0D3A5C;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 160,
+            child: RowTitleAmount(
+              lineRow: lineColor,
+              textTitle: 'Inversión empresarial',
+              height: 40,
+              titleSize: 12,
+              titleColorDark: titleColorDark,
+              titleColorLight: titleColorLight,
+              amountNumber: amountNumber,
+              amountSize: 14,
+              amountColorDark: amountColorDark,
+              amountColorLight: amountColorLight,
+              isLoader: false,
+            ),
+          ),
+          Image.asset(
+            "assets/reinvestment/reinvestment_image.png",
+            width: 40,
+            height: 40,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LabelText extends ConsumerWidget {
+  const LabelText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    const int labelDark = 0xff114E7C;
+    const int labelLight = 0xffA2E6FA;
+    return Positioned(
+      right: 0,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+          ),
+          color: isDarkMode ? const Color(labelDark) : const Color(labelLight),
+        ),
+        width: 112,
+        height: 24,
+        child: const Center(
+          child: TextPoppins(text: "Proceso por completar", fontSize: 8),
+        ),
+      ),
+    );
+  }
+}
