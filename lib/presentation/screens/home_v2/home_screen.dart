29cddd8baab6e040c111c69171ed9c4335608ae7
franco galbiati <francogalbiati984@gmail.com>
2024-08-12 10:04:55 -0300

    add ( slider_draft ) : add SliderDraft

--- lib/presentation/screens/home_v2/home_screen.dart
+++ lib/presentation/screens/home_v2/home_screen.dart
@@ -16,6 +16,7 @@ import 'package:finniu/presentation/screens/home_v2/widgets/all_investment_butto
 import 'package:finniu/presentation/screens/home_v2/widgets/custom_app_bar.dart';
 import 'package:finniu/presentation/screens/home_v2/widgets/funds_title.dart';
 import 'package:finniu/presentation/screens/home_v2/widgets/non_investmenr.dart';
+import 'package:finniu/presentation/screens/home_v2/widgets/slider_draft.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -171,6 +172,10 @@ class HomeBody extends HookConsumerWidget {
                         },
                       ),
                       const CarrouselSlider(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const SliderDraft(),
                     ],
                   ),
                   renderNonInvestment
---
+++ lib/presentation/screens/home_v2/widgets/slider_draft.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SliderDraft extends StatelessWidget {
+  const SliderDraft({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 90,
+      child: const Row(),
+    );
+  }
+}
