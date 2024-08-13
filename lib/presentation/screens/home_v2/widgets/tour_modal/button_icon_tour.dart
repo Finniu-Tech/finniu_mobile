ecf868a582c6390a4e7196c52be731ce756ede51
franco galbiati <francogalbiati984@gmail.com>
2024-08-13 12:59:00 -0300

    add ( images ) : add images tour

--- lib/presentation/screens/home_v2/widgets/tour_modal/button_icon_tour.dart
+++ lib/presentation/screens/home_v2/widgets/tour_modal/button_icon_tour.dart
@@ -41,3 +41,39 @@ class ButtonIconTour extends StatelessWidget {
     );
   }
 }
+
+class ButtonCloseTour extends StatelessWidget {
+  const ButtonCloseTour({
+    super.key,
+    required this.widthPercent,
+    required this.onPressed,
+    required this.label,
+  });
+  final double widthPercent;
+  final VoidCallback? onPressed;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    const int buttonColor = 0xff0E4D7D;
+    const int textButtonColor = 0xffFFFFFF;
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * widthPercent,
+      height: 40,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(
+            const Color(buttonColor),
+          ),
+        ),
+        onPressed: onPressed,
+        child: TextPoppins(
+          text: label,
+          fontSize: 16,
+          isBold: false,
+          textDark: textButtonColor,
+          textLight: textButtonColor,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/home_v2/widgets/tour_modal/state_tour.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class StateTour extends StatelessWidget {
+  const StateTour({
+    super.key,
+    required this.indexTour,
+    required this.items,
+  });
+  final int indexTour;
+  final int items;
+
+  @override
+  Widget build(BuildContext context) {
+    const int selectColor = 0xffA2E6FA;
+    const int unselectColor = 0xff0E4D7D;
+
+    return Center(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: List.generate(items, (index) {
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: indexTour == index
+                    ? const Color(selectColor)
+                    : const Color(unselectColor),
+              ),
+              width: 50,
+              height: 9,
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
