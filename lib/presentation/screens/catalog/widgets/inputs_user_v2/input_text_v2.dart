06c2bea4507ceba54a1fdc926b7042c97de952a9
franco galbiati <francogalbiati984@gmail.com>
2024-08-19 13:08:04 -0300

    add ( form_about ) : add InputTextFileUserProfile and svg icons

--- lib/presentation/screens/catalog/widgets/inputs_user_v2/input_text_v2.dart
+++ lib/presentation/screens/catalog/widgets/inputs_user_v2/input_text_v2.dart
@@ -1,5 +1,7 @@
 import 'package:finniu/presentation/providers/settings_provider.dart';
+import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class InputTextFileUserProfile extends ConsumerWidget {
@@ -73,6 +75,108 @@ class InputTextFileUserProfile extends ConsumerWidget {
             ),
             focusedErrorBorder: const OutlineInputBorder(
               borderRadius: BorderRadius.all(Radius.circular(25.0)),
+              borderSide: BorderSide.none,
+            ),
+          ),
+          validator: validator,
+          keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
+        ),
+        const SizedBox(height: 15),
+      ],
+    );
+  }
+}
+
+class InputTextNetworkUserProfile extends ConsumerWidget {
+  const InputTextNetworkUserProfile({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.validator,
+    required this.urlIcon,
+    required this.textTitle,
+    this.isNumeric = false,
+  });
+
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final String hintText;
+  final bool isNumeric;
+  final String urlIcon;
+  final String textTitle;
+
+  final int hintDark = 0xFF989898;
+  final int hintLight = 0xFF989898;
+  final int fillDark = 0xFF222222;
+  final int fillLight = 0xFFF7F7F7;
+  final int textDark = 0xFFFFFFFF;
+  final int textLight = 0xFF000000;
+  final int borderColorDark = 0xFFA2E6FA;
+  final int borderColorLight = 0xFF0D3A5C;
+  final int iconColorDark = 0xFFA2E6FA;
+  final int iconColorLight = 0xFF0D3A5C;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            SvgPicture.asset(
+              urlIcon,
+              width: 27,
+              height: 27,
+              color: isDarkMode ? Color(iconColorDark) : Color(iconColorLight),
+            ),
+            const SizedBox(width: 10),
+            TextPoppins(text: textTitle, fontSize: 12),
+          ],
+        ),
+        const SizedBox(height: 10),
+        TextFormField(
+          controller: controller,
+          style: TextStyle(
+            fontSize: 12,
+            color: isDarkMode ? Color(textDark) : Color(textLight),
+            fontWeight: FontWeight.w400,
+            fontFamily: "Poppins",
+          ),
+          decoration: InputDecoration(
+            hintStyle: TextStyle(
+              fontSize: 12,
+              color: isDarkMode ? Color(hintDark) : Color(hintLight),
+              fontWeight: FontWeight.w400,
+              fontFamily: "Poppins",
+            ),
+            hintText: hintText,
+            fillColor: isDarkMode ? Color(fillDark) : Color(fillLight),
+            filled: true,
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              borderSide: BorderSide.none,
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+              borderSide: BorderSide(
+                color: isDarkMode
+                    ? Color(borderColorDark)
+                    : Color(borderColorLight),
+                width: 1,
+              ),
+            ),
+            errorBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              borderSide: BorderSide.none,
+            ),
+            focusedErrorBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
               borderSide: BorderSide.none,
             ),
           ),
--- lib/presentation/screens/form_about_me_v2/form_about_me_v2.dart
+++ lib/presentation/screens/form_about_me_v2/form_about_me_v2.dart
@@ -1,9 +1,11 @@
+import 'package:finniu/presentation/screens/catalog/widgets/inputs_user_v2/input_text_v2.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/user_profil_v2/scafold_user_profile.dart';
 import 'package:finniu/presentation/screens/complete_details/widgets/app_bar_logo.dart';
 import 'package:finniu/presentation/screens/form_personal_data_v2/widgets/form_data_navigator.dart';
 import 'package:finniu/presentation/screens/form_personal_data_v2/widgets/progress_form.dart';
 import 'package:finniu/presentation/screens/form_personal_data_v2/widgets/title_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AboutMeDataV2 extends HookConsumerWidget {
@@ -12,9 +14,19 @@ class AboutMeDataV2 extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    final facebookTextController =
+        useTextEditingController(text: "https://facebook.com/");
+    final instagramTextController =
+        useTextEditingController(text: "https://instagram.com/");
+    final linkedinTextController =
+        useTextEditingController(text: "https://linkendIn.com/");
     void uploadJobData() {
       if (formKey.currentState!.validate()) {
         print("add personal data");
+        print(facebookTextController.text);
+        print(instagramTextController.text);
+        print(linkedinTextController.text);
       }
     }
 
@@ -47,6 +59,9 @@ class AboutMeDataV2 extends HookConsumerWidget {
           ),
           AboutMeForm(
             formKey: formKey,
+            facebookTextController: facebookTextController,
+            instagramTextController: instagramTextController,
+            linkedinTextController: linkedinTextController,
           ),
         ],
       ),
@@ -58,16 +73,44 @@ class AboutMeForm extends ConsumerWidget {
   const AboutMeForm({
     super.key,
     required this.formKey,
+    required this.facebookTextController,
+    required this.instagramTextController,
+    required this.linkedinTextController,
   });
   final GlobalKey<FormState> formKey;
+  final TextEditingController facebookTextController;
+  final TextEditingController instagramTextController;
+  final TextEditingController linkedinTextController;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Form(
       autovalidateMode: AutovalidateMode.disabled,
       key: formKey,
-      child: const Column(
-        children: [],
+      child: Column(
+        children: [
+          InputTextNetworkUserProfile(
+            urlIcon: "assets/svg_icons/facebook_icon_v2.svg",
+            textTitle: "Facebook",
+            controller: facebookTextController,
+            hintText: "Facebook",
+            validator: null,
+          ),
+          InputTextNetworkUserProfile(
+            urlIcon: "assets/svg_icons/instagram_icon_v2.svg",
+            textTitle: "Instagram",
+            controller: instagramTextController,
+            hintText: "Instagram",
+            validator: null,
+          ),
+          InputTextNetworkUserProfile(
+            urlIcon: "assets/svg_icons/linkedin_icon_v2.svg",
+            textTitle: "Linkedin",
+            controller: linkedinTextController,
+            hintText: "Linkedin",
+            validator: null,
+          ),
+        ],
       ),
     );
   }
