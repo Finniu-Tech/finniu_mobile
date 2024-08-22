d0d931365e9679c0287970b235a4370f469432be
franco galbiati <francogalbiati984@gmail.com>
2024-08-22 15:55:11 -0300

    add ( add_token ) : addToken function

--- lib/presentation/screens/activate_account_v2.dart/activate_account_v2.dart
+++ lib/presentation/screens/activate_account_v2.dart/activate_account_v2.dart
@@ -3,12 +3,14 @@ import 'package:finniu/presentation/providers/otp_provider.dart';
 import 'package:finniu/presentation/providers/timer_counterdown_provider.dart';
 import 'package:finniu/presentation/providers/user_provider.dart';
 import 'package:finniu/presentation/screens/activate_account_v2.dart/widgets/verification_code_v2.dart';
+import 'package:finniu/presentation/screens/catalog/circular_loader.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/send_proof_button.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/user_profil_v2/scafold_user_profile.dart';
 import 'package:finniu/widgets/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class ActivateAccountV2 extends HookConsumerWidget {
   const ActivateAccountV2({
@@ -47,86 +49,97 @@ class ActivateAccountV2 extends HookConsumerWidget {
       );
     }
 
-    return ScaffoldUserProfile(
-      children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.9,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const TextPoppins(
-                text: "Activa tu cuenta en segundos",
-                fontSize: 24,
-                textDark: titleDark,
-                textLight: titleLight,
-                lines: 2,
-                align: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Image.asset(
-                'assets/images/padlock.png',
-                width: 100,
-                height: 100,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const TextPoppins(
-                text: "Ingresa el código de verificación",
-                fontSize: 16,
-                textDark: subTitleDark,
-                textLight: subTitleLight,
-                isBold: true,
-                align: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const TextPoppins(
-                text:
-                    "Te hemos enviado el código a tu correo para confirmar la operación",
-                fontSize: 12,
-                textDark: subTitleDark,
-                textLight: subTitleLight,
-                isBold: false,
-                align: TextAlign.center,
-                lines: 2,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const VerificationCodeV2(),
-              const SizedBox(
-                height: 20,
-              ),
-              if (ref.watch(timerCounterDownProvider) == 0) ...[
-                ButtonInvestment(
-                  text: "Reenviar correo",
-                  onPressed: () async => sendCode(),
+    return LoaderOverlay(
+      useDefaultLoading: false,
+      overlayWidgetBuilder: (progress) {
+        return const Center(
+          child: CircularLoader(
+            width: 50,
+            height: 50,
+          ),
+        );
+      },
+      child: ScaffoldUserProfile(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const TextPoppins(
+                  text: "Activa tu cuenta en segundos",
+                  fontSize: 24,
+                  textDark: titleDark,
+                  textLight: titleLight,
+                  lines: 2,
+                  align: TextAlign.center,
                 ),
                 const SizedBox(
-                  height: 15,
+                  height: 20,
                 ),
-              ] else ...[
+                Image.asset(
+                  'assets/images/padlock.png',
+                  width: 100,
+                  height: 100,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
                 const TextPoppins(
-                  text: "Reenviar el código en ",
-                  fontSize: 11,
+                  text: "Ingresa el código de verificación",
+                  fontSize: 16,
+                  textDark: subTitleDark,
+                  textLight: subTitleLight,
+                  isBold: true,
+                  align: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const TextPoppins(
+                  text:
+                      "Te hemos enviado el código a tu correo para confirmar la operación",
+                  fontSize: 12,
                   textDark: subTitleDark,
                   textLight: subTitleLight,
                   isBold: false,
                   align: TextAlign.center,
+                  lines: 2,
                 ),
                 const SizedBox(
-                  height: 10,
+                  height: 20,
                 ),
-                const CircularCountdownV2(),
+                const VerificationCodeV2(),
+                const SizedBox(
+                  height: 20,
+                ),
+                if (ref.watch(timerCounterDownProvider) == 0) ...[
+                  ButtonInvestment(
+                    text: "Reenviar correo",
+                    onPressed: () async => sendCode(),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                ] else ...[
+                  const TextPoppins(
+                    text: "Reenviar el código en ",
+                    fontSize: 11,
+                    textDark: subTitleDark,
+                    textLight: subTitleLight,
+                    isBold: false,
+                    align: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const CircularCountdownV2(),
+                ],
               ],
-            ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/presentation/screens/activate_account_v2.dart/widgets/verification_code_v2.dart
+++ lib/presentation/screens/activate_account_v2.dart/widgets/verification_code_v2.dart
@@ -1,20 +1,14 @@
 import 'dart:async';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:finniu/constants/colors.dart';
-import 'package:finniu/infrastructure/models/auth.dart';
-import 'package:finniu/infrastructure/models/otp.dart';
-import 'package:finniu/infrastructure/repositories/auth_repository_imp.dart';
-import 'package:finniu/presentation/providers/auth_provider.dart';
-import 'package:finniu/presentation/providers/graphql_provider.dart';
-import 'package:finniu/presentation/providers/otp_provider.dart';
 import 'package:finniu/presentation/providers/settings_provider.dart';
 import 'package:finniu/presentation/providers/timer_counterdown_provider.dart';
-import 'package:finniu/presentation/providers/user_provider.dart';
+import 'package:finniu/presentation/screens/activate_account_v2.dart/helpers/add_token.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
-import 'package:finniu/widgets/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_verification_code/flutter_verification_code.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class VerificationCodeV2 extends HookConsumerWidget {
   const VerificationCodeV2({super.key});
@@ -22,73 +16,11 @@ class VerificationCodeV2 extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeProvider = ref.watch(settingsNotifierProvider);
-    final userProfile = ref.watch(userProfileNotifierProvider);
+    // final userProfile = ref.watch(userProfileNotifierProvider);
 
     void onComplete(String code) async {
-      final futureIsValidCode = ref.watch(
-        otpValidatorFutureProvider(
-          OTPForm(
-            email: userProfile.email!,
-            otp: code,
-            action: 'register',
-          ),
-        ).future,
-      );
-      futureIsValidCode.then((status) async {
-        if (status == true) {
-          final userProfile = ref.watch(userProfileNotifierProvider);
-
-          final login = LoginModel(
-            email: userProfile.email!,
-            password: userProfile.password!,
-          );
-
-          final graphqlProvider = ref.watch(gqlClientProvider.future);
-          final loginResponse = AuthRepository().login(
-            client: await graphqlProvider,
-            username: login.email.toLowerCase(),
-            password: login.password,
-          );
-          loginResponse.then((value) {
-            if (value.success) {
-              final token = ref.watch(
-                authTokenMutationProvider(
-                  LoginModel(
-                    email: login.email.toLowerCase(),
-                    password: login.password,
-                  ),
-                ).future,
-              );
-
-              token.then((value) {
-                if (value != null) {
-                  ref.read(authTokenProvider.notifier).state = value;
-                  Navigator.pushNamed(context, '/v2/form_personal_data');
-                } else {
-                  CustomSnackbar.show(
-                    context,
-                    "Error al procesar la solicitud token error",
-                    'error',
-                  );
-                }
-              });
-            } else {
-              CustomSnackbar.show(
-                context,
-                "Error al procesar la solicitud d loginResponse false",
-                'error',
-              );
-            }
-          });
-        } else {
-          Navigator.of(context).pop();
-          CustomSnackbar.show(
-            context,
-            'No se pudo validar el código de verificación',
-            'error',
-          );
-        }
-      });
+      context.loaderOverlay.show();
+      addToken(context, ref, code);
     }
 
     return Center(
