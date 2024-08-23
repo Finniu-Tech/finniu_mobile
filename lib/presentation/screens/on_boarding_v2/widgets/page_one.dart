d1cea61e45f34377d7c3075e96d10290283c48cc
franco galbiati <francogalbiati984@gmail.com>
2024-08-23 13:17:36 -0300

    add ( on_boarding ) : add PageOne

--- lib/presentation/screens/on_boarding_v2/widgets/page_one.dart
+++ lib/presentation/screens/on_boarding_v2/widgets/page_one.dart
@@ -1,4 +1,5 @@
 import 'package:finniu/presentation/providers/settings_provider.dart';
+import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -20,6 +21,9 @@ class PageOneContainer extends ConsumerWidget {
       Color(0xFFA1D6FB),
       Color(0xFFA2E6FA),
     ];
+    const String text = "Empieza a vivir una nueva experiencia con  Finniu ";
+    const int textDark = 0xffFFFFFF;
+    const int textLight = 0xff0D3A5C;
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
@@ -30,6 +34,37 @@ class PageOneContainer extends ConsumerWidget {
           end: Alignment.bottomCenter,
           stops: const [0.0, 0.5, 1.0],
         ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/onboarding/logo_onboarding_${isDarkMode ? "dark" : "light"}.png",
+            width: 200,
+            height: 180,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const SizedBox(
+            width: 250,
+            child: TextPoppins(
+              text: text,
+              fontSize: 16,
+              isBold: true,
+              lines: 2,
+              align: TextAlign.center,
+              textDark: textDark,
+              textLight: textLight,
+            ),
+          ),
+          Icon(
+            Icons.arrow_forward,
+            size: 24,
+            color: isDarkMode ? const Color(textDark) : const Color(textLight),
+          ),
+        ],
       ),
     );
   }
