fd5968dd56283afeb762a7f1ab84b70c3a6310d4
franco galbiati <francogalbiati984@gmail.com>
2024-08-27 15:03:13 -0300

    add ( profile_v2 ) : add Scaffold , AppBarProfile and images

--- lib/presentation/screens/profile_v2/profile_screen_v2.dart
+++ lib/presentation/screens/profile_v2/profile_screen_v2.dart
@@ -1,4 +1,7 @@
 import 'package:finniu/presentation/providers/settings_provider.dart';
+import 'package:finniu/presentation/providers/user_provider.dart';
+import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
+import 'package:finniu/presentation/screens/profile_v2/widgets/app_bar_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -7,57 +10,94 @@ class UserProfileV2 extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      appBar: AppBarProfile(title: "Mi perfil"),
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    const int backgroundDark = 0xff191919;
+    const int backgroundLight = 0xffFFFFFF;
+    return Scaffold(
+      appBar: const AppBarProfile(title: "Mi perfil"),
+      backgroundColor: isDarkMode
+          ? const Color(backgroundDark)
+          : const Color(backgroundLight),
+      body: const SingleChildScrollView(
+        child: _BodyProfile(),
+      ),
     );
   }
 }
 
-class AppBarProfile extends ConsumerWidget implements PreferredSizeWidget {
-  const AppBarProfile({
+class _BodyProfile extends ConsumerWidget {
+  const _BodyProfile({
     super.key,
-    required this.title,
   });
-  final String title;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userProfile = ref.watch(userProfileNotifierProvider);
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ImageProfileStack(),
+      ],
+    );
+  }
+}
+
+class ImageProfileStack extends ConsumerWidget {
+  const ImageProfileStack({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
     const int backgroundDark = 0xff191919;
     const int backgroundLight = 0xffFFFFFF;
-    const int titleColorDark = 0xffFFFFFF;
-    const int titleColorLight = 0xff000000;
-    const int iconColorDark = 0xffA2E6FA;
-    const int iconColorLight = 0xff0D3A5C;
-
-    return AppBar(
-      centerTitle: true,
-      backgroundColor: isDarkMode
-          ? const Color(backgroundDark)
-          : const Color(backgroundLight),
-      automaticallyImplyLeading: true,
-      title: Text(
-        title,
-        style: TextStyle(
-          fontFamily: "Poppins",
-          fontSize: 17,
-          color: isDarkMode
-              ? const Color(titleColorDark)
-              : const Color(titleColorLight),
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      leading: IconButton(
-        onPressed: () => Navigator.pop(context),
-        icon: Icon(
-          Icons.arrow_back,
-          color: isDarkMode
-              ? const Color(iconColorDark)
-              : const Color(iconColorLight),
-        ),
+    return SizedBox(
+      height: 200,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Image.asset(
+            "assets/backgroud/profile_background_${isDarkMode ? "dark" : "light"}.png",
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            fit: BoxFit.fill,
+          ),
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextPoppins(text: "mariana", fontSize: 16, isBold: true),
+              TextPoppins(text: "mariana", fontSize: 15, isBold: true),
+            ],
+          ),
+          Positioned(
+            top: 50,
+            width: MediaQuery.of(context).size.width / 2,
+            child: Container(
+              width: 90,
+              height: 90,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: isDarkMode
+                      ? const Color(backgroundDark)
+                      : const Color(backgroundLight),
+                  width: 10,
+                ),
+              ),
+              child: ClipOval(
+                child: Image.asset(
+                  "assets/avatars/profile_image_${isDarkMode ? "dark" : "light"}.png",
+                  width: 80,
+                  height: 80,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 1.0);
 }
