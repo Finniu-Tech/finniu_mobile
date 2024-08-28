e9587582b75596ae59f507308cca1b04df8446d2
franco galbiati <francogalbiati984@gmail.com>
2024-08-28 10:44:02 -0300

    add ( profile_v2 ) :  add ImageProfileStack

--- lib/presentation/screens/profile_v2/profile_screen_v2.dart
+++ lib/presentation/screens/profile_v2/profile_screen_v2.dart
@@ -1,7 +1,6 @@
 import 'package:finniu/presentation/providers/settings_provider.dart';
-import 'package:finniu/presentation/providers/user_provider.dart';
-import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:finniu/presentation/screens/profile_v2/widgets/app_bar_profile.dart';
+import 'package:finniu/presentation/screens/profile_v2/widgets/image_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -26,78 +25,25 @@ class UserProfileV2 extends ConsumerWidget {
 }
 
 class _BodyProfile extends ConsumerWidget {
-  const _BodyProfile({
-    super.key,
-  });
+  const _BodyProfile();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userProfile = ref.watch(userProfileNotifierProvider);
+    // final userProfile = ref.watch(userProfileNotifierProvider);
+
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ImageProfileStack(),
+        ImageProfileStack(
+          fullName: "Mariana",
+          email: "JUQpC@example.com",
+          profileImage:
+              "https://img.lovepik.com/free-png/20220121/lovepik-creative-owl-animal-small-avatar-art-illustration-png-image_401570099_wh860.png",
+          backgroundImage:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-8W1UenncOIyiL8R0a2TZfGB0H3jXgKnIww&s",
+        ),
       ],
-    );
-  }
-}
-
-class ImageProfileStack extends ConsumerWidget {
-  const ImageProfileStack({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
-    const int backgroundDark = 0xff191919;
-    const int backgroundLight = 0xffFFFFFF;
-    return SizedBox(
-      height: 200,
-      child: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          Image.asset(
-            "assets/backgroud/profile_background_${isDarkMode ? "dark" : "light"}.png",
-            width: MediaQuery.of(context).size.width,
-            height: 100,
-            fit: BoxFit.fill,
-          ),
-          const Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextPoppins(text: "mariana", fontSize: 16, isBold: true),
-              TextPoppins(text: "mariana", fontSize: 15, isBold: true),
-            ],
-          ),
-          Positioned(
-            top: 50,
-            width: MediaQuery.of(context).size.width / 2,
-            child: Container(
-              width: 90,
-              height: 90,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: isDarkMode
-                      ? const Color(backgroundDark)
-                      : const Color(backgroundLight),
-                  width: 10,
-                ),
-              ),
-              child: ClipOval(
-                child: Image.asset(
-                  "assets/avatars/profile_image_${isDarkMode ? "dark" : "light"}.png",
-                  width: 80,
-                  height: 80,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
