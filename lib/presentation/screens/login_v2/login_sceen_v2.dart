71039bbd85d686db975c2839031a3a23e78c929e
franco galbiati <francogalbiati984@gmail.com>
2024-09-13 10:48:21 -0300

    add ( login_v2 ) : add LoginScreenV2

---
+++ lib/presentation/screens/login_v2/login_sceen_v2.dart
@@ -0,0 +1,14 @@
+import 'package:finniu/presentation/screens/catalog/widgets/user_profil_v2/scafold_user_profile.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreenV2 extends StatelessWidget {
+  const LoginScreenV2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ScaffoldUserProfile(
+      appBar: null,
+      children: [],
+    );
+  }
+}
--- lib/presentation/screens/profile_v2/profile_screen_v2.dart
+++ lib/presentation/screens/profile_v2/profile_screen_v2.dart
@@ -39,7 +39,7 @@ class _BodyProfile extends ConsumerWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         ImageProfileStack(
-          fullName: "${userProfile.firstName} ${userProfile.lastName}",
+          fullName: "${userProfile.firstName} ${userProfile.lastName ?? ''}",
           email: "${userProfile.email}",
           profileImage: "${userProfile.imageProfileUrl}",
           backgroundImage: "",
