20b11fa9be995f0ffd97e00635e497447a14c5bf
Harol Calzada <harol.calzada@gmail.com>
2024-09-18 11:58:56 -0500

    add aviable biometrics function

--- lib/presentation/screens/biometric_screen.dart
+++ lib/presentation/screens/biometric_screen.dart
@@ -6,7 +6,13 @@ class BiometricTestPage extends StatelessWidget {
 
   Future<void> _authenticate(BuildContext context) async {
     bool canCheckBiometrics = await localAuth.canCheckBiometrics;
+    bool isDeviceSupported = await localAuth.isDeviceSupported();
     print('canCheckBiometrics: $canCheckBiometrics');
+    print('isDeviceSupported: $isDeviceSupported');
+
+    final List<BiometricType> availableBiometrics = await localAuth.getAvailableBiometrics();
+
+    print('availableBiometrics: $availableBiometrics');
 
     if (canCheckBiometrics) {
       try {
@@ -36,9 +42,22 @@ class BiometricTestPage extends StatelessWidget {
         );
       }
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Biometría no disponible en este dispositivo')),
-      );
+      if (!isDeviceSupported) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Dispositivo no soportado')),
+        );
+      } else {
+        bool didAuthenticate = await localAuth.authenticate(
+          localizedReason: 'Por favor, autentícate con tu password para acceder',
+          options: const AuthenticationOptions(
+            useErrorDialogs: true,
+            stickyAuth: true,
+          ),
+        );
+      }
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   SnackBar(content: Text('Biometría no disponible en este dispositivo')),
+      // );
     }
   }
 
