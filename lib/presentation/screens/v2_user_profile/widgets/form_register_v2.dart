0ac14ef5c334539f1f60a1a9f5c6bbe188c2942d
franco galbiati <francogalbiati984@gmail.com>
2024-09-20 12:52:53 -0300

    minor fix

--- lib/presentation/screens/v2_user_profile/widgets/form_register_v2.dart
+++ lib/presentation/screens/v2_user_profile/widgets/form_register_v2.dart
@@ -1,5 +1,4 @@
 import 'package:finniu/infrastructure/models/user_profile_v2/profile_form_dto.dart';
-import 'package:finniu/presentation/providers/settings_provider.dart';
 import 'package:finniu/presentation/screens/catalog/helpers/inputs_user_helpers_v2.dart/helper_register_form.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/inputs_user_v2/check_terms_conditions.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/inputs_user_v2/input_password_v2.dart';
@@ -9,6 +8,7 @@ import 'package:finniu/presentation/screens/catalog/widgets/send_proof_button.da
 import 'package:finniu/presentation/screens/catalog/widgets/snackbar/snackbar_v2.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:finniu/presentation/screens/v2_user_profile/helpers/validate_form.dart';
+import 'package:finniu/presentation/screens/v2_user_profile/widgets/password_required.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -225,168 +225,6 @@ class FormRegister extends HookConsumerWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class PasswordRequired extends HookConsumerWidget {
-  const PasswordRequired({
-    super.key,
-    required this.controller,
-    required this.isExpanded,
-  });
-  final bool isExpanded;
-  final TextEditingController controller;
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
-    const int containerDark = 0xff222222;
-    const int containerLight = 0xffF7F7F7;
-
-    final ValueNotifier<bool> eightCharacters = ValueNotifier<bool>(false);
-    final ValueNotifier<bool> oneNumber = ValueNotifier<bool>(false);
-    final ValueNotifier<bool> oneLowercase = ValueNotifier<bool>(false);
-    final ValueNotifier<bool> oneCapital = ValueNotifier<bool>(false);
-    final ValueNotifier<bool> oneSpecialSymbol = ValueNotifier<bool>(false);
-
-    final RegExp hasDigits = RegExp(r'[0-9]');
-    final RegExp hasLowercase = RegExp(r'[a-z]');
-    final RegExp hasUppercase = RegExp(r'[A-Z]');
-    final RegExp hasSpecialCharacters = RegExp(r'[!@#$%^&*(),.?":{}|<>]');
-
-    useEffect(
-      () {
-        controller.addListener(() {
-          controller.text.length >= 8
-              ? eightCharacters.value = true
-              : eightCharacters.value = false;
-          hasDigits.hasMatch(controller.text)
-              ? oneNumber.value = true
-              : oneNumber.value = false;
-          hasLowercase.hasMatch(controller.text)
-              ? oneLowercase.value = true
-              : oneLowercase.value = false;
-          hasUppercase.hasMatch(controller.text)
-              ? oneCapital.value = true
-              : oneCapital.value = false;
-          hasSpecialCharacters.hasMatch(controller.text)
-              ? oneSpecialSymbol.value = true
-              : oneSpecialSymbol.value = false;
-        });
-        return null;
-      },
-      [controller],
-    );
-
-    return Positioned(
-      top: 250,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 500),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: isDarkMode
-              ? const Color(containerDark)
-              : const Color(containerLight),
-        ),
-        width: MediaQuery.of(context).size.width * 0.9,
-        height: isExpanded ? 200 : 0,
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ValueListenableBuilder<bool>(
-              valueListenable: eightCharacters,
-              builder: (context, value, child) {
-                return ItemValidate(
-                  text: "Al menos 8 caracteres",
-                  isValidate: eightCharacters.value,
-                );
-              },
-            ),
-            ValueListenableBuilder<bool>(
-              valueListenable: oneNumber,
-              builder: (context, value, child) {
-                return ItemValidate(
-                  text: "Al menos 1 número (0....9)",
-                  isValidate: oneNumber.value,
-                );
-              },
-            ),
-            ValueListenableBuilder<bool>(
-              valueListenable: oneLowercase,
-              builder: (context, value, child) {
-                return ItemValidate(
-                  text: "Al menos 1 letra minúscula (a...z)",
-                  isValidate: oneLowercase.value,
-                );
-              },
-            ),
-            ValueListenableBuilder<bool>(
-              valueListenable: oneCapital,
-              builder: (context, value, child) {
-                return ItemValidate(
-                  text: "Al menos 1 letra mayúscula (A...Z)",
-                  isValidate: oneCapital.value,
-                );
-              },
-            ),
-            ValueListenableBuilder<bool>(
-              valueListenable: oneSpecialSymbol,
-              builder: (context, value, child) {
-                return ItemValidate(
-                  text: "Al menos 1 símbolo especial (!...\$)",
-                  isValidate: oneSpecialSymbol.value,
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ItemValidate extends ConsumerWidget {
-  const ItemValidate({
-    super.key,
-    required this.text,
-    required this.isValidate,
-  });
-  final String text;
-  final bool isValidate;
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
-    const iconTrueDark = 0xff0DCB6C;
-    const iconTrueLight = 0xff09A554;
-    const iconFalseDark = 0xffFF5C62;
-    const iconFalseLight = 0xffED1C24;
-
-    return Row(
-      children: [
-        isValidate
-            ? Icon(
-                Icons.check_circle_outline,
-                color: isDarkMode
-                    ? const Color(iconTrueDark)
-                    : const Color(iconTrueLight),
-                size: 20,
-              )
-            : Icon(
-                Icons.error_outline,
-                color: isDarkMode
-                    ? const Color(iconFalseDark)
-                    : const Color(iconFalseLight),
-                size: 20,
-              ),
-        const SizedBox(
-          width: 5,
-        ),
-        TextPoppins(
-          text: text,
-          fontSize: 12,
-        ),
-      ],
     );
   }
 }
