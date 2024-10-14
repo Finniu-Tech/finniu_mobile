de98aab233c6c5b36fe177007f248c9ff21d778c
Harol Calzada <harol.calzada@gmail.com>
2024-10-14 10:17:40 -0500

    add test screen for push notifications

--- lib/presentation/screens/catalog/catalog_screen.dart
+++ lib/presentation/screens/catalog/catalog_screen.dart
@@ -54,9 +54,7 @@ class CatalogScreen extends HookConsumerWidget {
     const int backgroundDark = 0xff191919;
     const int backgroundLight = 0xffFFFFFF;
     return Scaffold(
-      backgroundColor: isDarkMode
-          ? const Color(backgroundDark)
-          : const Color(backgroundLight),
+      backgroundColor: isDarkMode ? const Color(backgroundDark) : const Color(backgroundLight),
       bottomNavigationBar: const NavigationBarHome(),
       appBar: AppBar(
         elevation: 0.0,
@@ -136,8 +134,7 @@ class CatalogScreen extends HookConsumerWidget {
                 showSnackBarV2(
                   context: context,
                   title: "probando",
-                  message:
-                      "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+                  message: "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
                   snackType: SnackType.error,
                 );
               },
@@ -151,8 +148,7 @@ class CatalogScreen extends HookConsumerWidget {
                 showSnackBarV2(
                   context: context,
                   title: "probando",
-                  message:
-                      "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+                  message: "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
                   snackType: SnackType.warning,
                 );
               },
@@ -166,8 +162,7 @@ class CatalogScreen extends HookConsumerWidget {
                 showSnackBarV2(
                   context: context,
                   title: "probando",
-                  message:
-                      "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+                  message: "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
                   snackType: SnackType.success,
                 );
               },
@@ -181,8 +176,7 @@ class CatalogScreen extends HookConsumerWidget {
                 showSnackBarV2(
                   context: context,
                   title: "probando",
-                  message:
-                      "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+                  message: "estoy probandossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
                   snackType: SnackType.info,
                 );
               },
@@ -199,6 +193,15 @@ class CatalogScreen extends HookConsumerWidget {
             const SizedBox(
               height: 10,
             ),
+            ButtonInvestment(
+              text: "push notification test",
+              onPressed: () {
+                Navigator.pushNamed(context, '/push_notification');
+              },
+            ),
+            SizedBox(
+              height: 10,
+            ),
             const Padding(
               padding: EdgeInsets.all(8.0),
               child: RowDownload(
@@ -209,8 +212,7 @@ class CatalogScreen extends HookConsumerWidget {
               title: "Verificación  legal",
               children: [
                 ChildrenCheckboxTitle(
-                  text:
-                      "Eres miembro o familiar de un funcionario público o una persona políticamente expuesta. ",
+                  text: "Eres miembro o familiar de un funcionario público o una persona políticamente expuesta. ",
                   value: true,
                 ),
               ],
@@ -234,8 +236,7 @@ class CatalogScreen extends HookConsumerWidget {
             const ExpansionTitleProfile(
               icon: "assets/svg_icons/dark_mode_icon.svg",
               title: "Contraseñas",
-              subtitle:
-                  "Sobre los depósitos, aprobaciones de mis inversiones y otros.",
+              subtitle: "Sobre los depósitos, aprobaciones de mis inversiones y otros.",
               children: [
                 ChildrenTitle(
                   title: "Visualización de contraseña",
@@ -253,8 +254,7 @@ class CatalogScreen extends HookConsumerWidget {
             ButtonSwitchProfile(
               icon: null,
               title: "Sobre mis inversiones",
-              subtitle:
-                  "Sobre los depósitos, aprobaciones de mis inversiones y otros.",
+              subtitle: "Sobre los depósitos, aprobaciones de mis inversiones y otros.",
               onTap: () => setInvest(),
               value: invest,
             ),
