dc4452da4a9281e6f89552e943bd03bd4e941bc1
franco galbiati <franco.galbiati@finniu.com>
2024-11-26 16:38:48 -0300

    add ( detail ) : add CarrouselDetailV4

--- lib/presentation/screens/home_v4/product_v4/carrousel_detail.dart
+++ lib/presentation/screens/home_v4/product_v4/carrousel_detail.dart
@@ -6,6 +6,7 @@ import 'package:finniu/presentation/screens/catalog/widgets/carrousel_slide.dart
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class CarrouselDetailV4 extends ConsumerWidget {
   const CarrouselDetailV4({super.key});
@@ -13,6 +14,11 @@ class CarrouselDetailV4 extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isSoles = ref.watch(isSolesStateProvider);
+    final data = [
+      {"x": "Ene 24", "y": 4.3},
+      {"x": "Feb 24", "y": 4.5},
+      {"x": "Mar 24", "y": 4.6},
+    ];
     final List<Widget> items = [
       ManagedAssetsV4(
         investmentsText: isSoles ? 5700000 : 570000,
@@ -23,6 +29,13 @@ class CarrouselDetailV4 extends ConsumerWidget {
         numberColorDark: 0xFF000000,
         numberColorLight: 0xFF000000,
         isSoles: isSoles,
+      ),
+      InvestedCapitalV4(
+        data: data,
+        cardColorDark: 0xff0A2F4A,
+        cardColorLight: 0xff0A2F4A,
+        columnColorDark: 0xff104872,
+        columnColorLight: 0xff104872,
       ),
     ];
 
@@ -100,6 +113,83 @@ class ManagedAssetsV4 extends ConsumerWidget {
             fontSize: 32,
             colorText: isDarkMode ? numberColorDark : numberColorLight,
             beginNumber: 0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InvestedCapitalV4 extends ConsumerWidget {
+  final List<dynamic> data;
+  const InvestedCapitalV4({
+    super.key,
+    required this.data,
+    required this.cardColorLight,
+    required this.cardColorDark,
+    required this.columnColorDark,
+    required this.columnColorLight,
+  });
+  final int cardColorLight;
+  final int cardColorDark;
+  final int columnColorDark;
+  final int columnColorLight;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    return SlideCarouselCard(
+      color: isDarkMode ? cardColorDark : cardColorLight,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const TextPoppins(
+            text: "Patrimonio neto (S/ MM)",
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            textDark: 0xffFFFFFF,
+            textLight: 0xffFFFFFF,
+          ),
+          SizedBox(
+            height: 170,
+            child: SfCartesianChart(
+              borderWidth: 0,
+              borderColor: Colors.transparent,
+              series: [
+                ColumnSeries(
+                  color: Color(isDarkMode ? columnColorDark : columnColorLight),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(5),
+                    topRight: Radius.circular(5),
+                  ),
+                  dataSource: data,
+                  xValueMapper: (datum, index) => datum["x"],
+                  yValueMapper: (datum, index) => datum["y"],
+                  dataLabelSettings: const DataLabelSettings(
+                    isVisible: true,
+                    labelAlignment: ChartDataLabelAlignment.auto,
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  borderWidth: 0,
+                ),
+              ],
+              primaryXAxis: const CategoryAxis(
+                majorGridLines: MajorGridLines(width: 0),
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              primaryYAxis: const CategoryAxis(
+                majorGridLines: MajorGridLines(width: 0),
+                isVisible: false,
+              ),
+            ),
           ),
         ],
       ),
