ccf6b5700ef96656860a0f48eec952da4ca370a5
franco galbiati <franco.galbiati@finniu.com>
2024-12-18 16:07:48 -0300

    change ( settings ) : change loader notifications switch

--- lib/presentation/screens/profile_v2/widgets/button_switch_profile.dart
+++ lib/presentation/screens/profile_v2/widgets/button_switch_profile.dart
@@ -1,4 +1,5 @@
 import 'package:finniu/presentation/providers/settings_provider.dart';
+import 'package:finniu/presentation/screens/catalog/circular_loader.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -34,7 +35,8 @@ class ButtonSwitchProfile extends ConsumerWidget {
         Divider(
           height: 1,
           thickness: 1,
-          color: isDarkMode ? const Color(dividerDark) : const Color(dividerLight),
+          color:
+              isDarkMode ? const Color(dividerDark) : const Color(dividerLight),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
@@ -47,14 +49,18 @@ class ButtonSwitchProfile extends ConsumerWidget {
               children: [
                 Column(
                   children: [
-                    icon == null ? const SizedBox.shrink() : const SizedBox(height: 15),
+                    icon == null
+                        ? const SizedBox.shrink()
+                        : const SizedBox(height: 15),
                     icon == null
                         ? const SizedBox.shrink()
                         : SvgPicture.asset(
                             icon!,
                             width: 25,
                             height: 25,
-                            color: isDarkMode ? const Color(iconDark) : const Color(iconLight),
+                            color: isDarkMode
+                                ? const Color(iconDark)
+                                : const Color(iconLight),
                           ),
                   ],
                 ),
@@ -94,7 +100,106 @@ class ButtonSwitchProfile extends ConsumerWidget {
         Divider(
           height: 1,
           thickness: 1,
-          color: isDarkMode ? const Color(dividerDark) : const Color(dividerLight),
+          color:
+              isDarkMode ? const Color(dividerDark) : const Color(dividerLight),
+        ),
+      ],
+    );
+  }
+}
+
+class ButtonSwitchProfileLoader extends ConsumerWidget {
+  const ButtonSwitchProfileLoader({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.onTap,
+    required this.value,
+    this.isExternalState,
+  });
+  final String? icon;
+  final String title;
+  final String subtitle;
+  final VoidCallback onTap;
+  final bool value;
+  final bool? isExternalState;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(settingsNotifierProvider).isDarkMode;
+    const int dividerDark = 0xff292828;
+    const int dividerLight = 0xffF6F6F6;
+    const int iconDark = 0xffA2E6FA;
+    const int iconLight = 0xff000000;
+
+    return Column(
+      children: [
+        Divider(
+          height: 1,
+          thickness: 1,
+          color:
+              isDarkMode ? const Color(dividerDark) : const Color(dividerLight),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 85,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    icon == null
+                        ? const SizedBox.shrink()
+                        : const SizedBox(height: 15),
+                    icon == null
+                        ? const SizedBox.shrink()
+                        : SvgPicture.asset(
+                            icon!,
+                            width: 25,
+                            height: 25,
+                            color: isDarkMode
+                                ? const Color(iconDark)
+                                : const Color(iconLight),
+                          ),
+                  ],
+                ),
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextPoppins(
+                        text: title,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        align: TextAlign.start,
+                      ),
+                      TextPoppins(
+                        text: subtitle,
+                        fontSize: 12,
+                        lines: 2,
+                        align: TextAlign.start,
+                      ),
+                    ],
+                  ),
+                ),
+                const Center(
+                  child: CircularLoader(width: 25, height: 25),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Divider(
+          height: 1,
+          thickness: 1,
+          color:
+              isDarkMode ? const Color(dividerDark) : const Color(dividerLight),
         ),
       ],
     );
