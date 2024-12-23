8a7e2a91766e29f15dbb9225b5f5718b1ee6aa6b
franco galbiati <franco.galbiati@finniu.com>
2024-12-23 10:06:31 -0300

    add ( money ) : add cahnge in money

--- lib/presentation/screens/home_v4/money_withdrawal/money_withdrawal.dart
+++ lib/presentation/screens/home_v4/money_withdrawal/money_withdrawal.dart
@@ -1,9 +1,9 @@
-import 'package:finniu/constants/number_format.dart';
 import 'package:finniu/domain/entities/fund_entity.dart';
 import 'package:finniu/domain/entities/re_investment_entity.dart';
 import 'package:finniu/presentation/providers/get_fund_investment.dart';
 import 'package:finniu/presentation/providers/money_provider.dart';
 import 'package:finniu/presentation/providers/settings_provider.dart';
+import 'package:finniu/presentation/screens/catalog/widgets/animated_number.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/send_proof_button.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/text_poppins.dart';
 import 'package:finniu/presentation/screens/catalog/widgets/user_profil_v2/scafold_user_profile.dart';
@@ -52,16 +52,18 @@ class MoneyBody extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isDarkMode = ref.read(settingsNotifierProvider).isDarkMode;
     final isSoles = ref.read(isSolesStateProvider);
-    const String title = "¡Gracias por depositar tu confianza en Finniu!";
-    const String subTitle =
-        "Si reinviertes, tu inversión crecerá en 1 año a este monto 👇🏻";
-    final String amount = isSoles
-        ? formatterSolesNotComma.format(args.amount * 1.20)
-        : formatterUSDNotComma.format(args.amount * 1.20);
     const int titleDark = 0xffFFFFFF;
     const int titleLight = 0xff0D3A5C;
     const int amountDark = 0xffA2E6FA;
     const int amountLight = 0xff0D3A5C;
+
+    const String title =
+        "¿Sabías que al quedarte con nosotros podrías duplicar tus ganancias?";
+    const String timeInvest = "Hace un año invertiste ";
+
+    final int amountNum = args.amount;
+    const int rentNum = 200;
+    const int reInvestRentNum = 400;
 
     void navigateToReinvest() async {
       context.loaderOverlay.show();
@@ -86,7 +88,9 @@ class MoneyBody extends ConsumerWidget {
     return Center(
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.85,
-        height: MediaQuery.of(context).size.height - 100,
+        height: MediaQuery.of(context).size.height > 650
+            ? MediaQuery.of(context).size.height - 100
+            : 620,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -94,28 +98,83 @@ class MoneyBody extends ConsumerWidget {
             const Spacer(),
             const TextPoppins(
               text: title,
-              fontSize: 20,
+              fontSize: 18,
               fontWeight: FontWeight.w600,
               align: TextAlign.center,
-              lines: 2,
+              lines: 3,
               textDark: titleDark,
               textLight: titleLight,
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 5),
             const TextPoppins(
-              text: subTitle,
-              fontSize: 16,
+              text: timeInvest,
+              fontSize: 14,
               align: TextAlign.center,
               lines: 2,
             ),
-            const SizedBox(height: 10),
-            TextPoppins(
-              text: amount,
-              fontSize: 40,
+            const SizedBox(height: 5),
+            AnimationNumberNotComma(
+              endNumber: amountNum,
+              fontSize: 32,
+              duration: 1,
+              colorText: isDarkMode ? amountDark : amountLight,
+              beginNumber: 0,
+              isSoles: isSoles,
+            ),
+            const SizedBox(height: 5),
+            const TextPoppins(
+              text: "con nosotros y ya has ganado",
+              fontSize: 14,
+              align: TextAlign.center,
+              lines: 2,
+            ),
+            const SizedBox(height: 5),
+            AnimationNumberNotComma(
+              endNumber: rentNum,
+              fontSize: 32,
+              duration: 1,
+              colorText: isDarkMode ? amountDark : amountLight,
+              beginNumber: 0,
+              isSoles: isSoles,
+            ),
+            const SizedBox(height: 5),
+            const TextPoppins(
+              text: "¡Felicidades por tomar esa gran decisión!",
+              fontSize: 16,
               fontWeight: FontWeight.w600,
               align: TextAlign.center,
-              textDark: amountDark,
-              textLight: amountLight,
+              lines: 2,
+            ),
+            const SizedBox(height: 5),
+            const TextPoppins(
+              text:
+                  "Si decides reinvertir este año, no solo conservarás tus ganancias, sino que podrías ganar hasta",
+              fontSize: 14,
+              align: TextAlign.center,
+              lines: 3,
+            ),
+            const SizedBox(height: 5),
+            AnimationNumberNotComma(
+              endNumber: reInvestRentNum,
+              fontSize: 32,
+              duration: 1,
+              colorText: isDarkMode ? amountDark : amountLight,
+              beginNumber: 0,
+              isSoles: isSoles,
+            ),
+            const SizedBox(height: 5),
+            const TextPoppins(
+              text: "más en los próximos 12 meses.",
+              fontSize: 14,
+              align: TextAlign.center,
+              lines: 3,
+            ),
+            const TextPoppins(
+              text:
+                  "Dale a tu inversión la oportunidad de llegar más lejos. ¡Reinvierte hoy mismo y sigue avanzando hacia tus metas financieras!”",
+              fontSize: 14,
+              align: TextAlign.center,
+              lines: 3,
             ),
             const Spacer(),
             ButtonInvestment(
@@ -127,6 +186,7 @@ class MoneyBody extends ConsumerWidget {
               text: "Solicitar mi retiro",
               onPressed: () => showModalReasons(context, isDarkMode, args.uuid),
             ),
+            const SizedBox(height: 10),
           ],
         ),
       ),
